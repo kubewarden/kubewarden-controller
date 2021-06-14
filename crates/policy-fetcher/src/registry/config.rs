cf48db13d8cb9bbeaf5f9a0fcb86ae24cce8bf33
Rafael Fernández López <ereslibre@ereslibre.es>
2021-06-14 10:23:56 +0200

    feat: ignore invalid host configuration on the dockerconfigjson
    `kubectl` has generated docker config json files that miss the `auth`
    key in the `auths` field [1].
    
    Due to this, make the parsing of the dockerconfigjson file less
    strict, with an optional `auth` field. If this field is not present,
    the host will be ignored, but the rest of the file will apply.
    
    We keep the same behavior of a complete failure in the following
    cases:
    
    - The JSON file is not syntactically valid
    - The basic auth information is not valid base64
    - The decoded base64 is not in the form `username:password`.
    
    [1] https://github.com/kubernetes/kubectl/issues/571

--- crates/policy-fetcher/src/registry/config.rs
+++ crates/policy-fetcher/src/registry/config.rs
@@ -4,7 +4,12 @@ use std::{collections::HashMap, convert::TryFrom, convert::TryInto, fs::File, pa
 
 #[derive(Deserialize, Debug)]
 pub(crate) struct RegistryAuthRaw {
-    auth: String,
+    // `auth` is optional because we have to be liberal on what we
+    // accept: a tool or a user might change the configuration file in
+    // disk and end up with a syntactically valid JSON, but
+    // semantically invalid. Check:
+    // https://github.com/kubernetes/kubectl/issues/571
+    auth: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -30,30 +35,83 @@ impl TryFrom<DockerConfigRaw> for DockerConfig {
             auths: docker_config
                 .auths
                 .into_iter()
-                .map(|(host, auth)| Ok((host, RegistryAuth::try_from(auth)?)))
+                .filter_map(|(host, auth)| match OptionalRegistryAuth::try_from(auth) {
+                    Ok(registry_auth) => {
+                        registry_auth.map(|registry_auth| Ok((host, registry_auth)))
+                    }
+                    Err(e) => {
+                        eprintln!(
+                            "error parsing host {} configuration: {}; host ignored",
+                            host, e
+                        );
+                        None
+                    }
+                })
                 .collect::<Result<_>>()?,
         })
     }
 }
 
-impl TryFrom<RegistryAuthRaw> for RegistryAuth {
+type OptionalRegistryAuth = Option<RegistryAuth>;
+
+impl TryFrom<RegistryAuthRaw> for OptionalRegistryAuth {
     type Error = anyhow::Error;
 
     fn try_from(auth: RegistryAuthRaw) -> Result<Self> {
-        if let Ok(basic_auth) = base64::decode(auth.auth) {
-            let splitted: Vec<&[u8]> = basic_auth.split(|c| *c == b':').collect();
-            if splitted.len() == 2 {
-                let (username, password) = (splitted[0], splitted[1]);
-                Ok(RegistryAuth::BasicAuth(username.into(), password.into()))
+        if let Some(auth) = auth.auth {
+            if let Ok(basic_auth) = base64::decode(auth) {
+                let splitted: Vec<&[u8]> = basic_auth.split(|c| *c == b':').collect();
+                if splitted.len() == 2 {
+                    let (username, password) = (splitted[0], splitted[1]);
+                    Ok(Some(RegistryAuth::BasicAuth(
+                        username.into(),
+                        password.into(),
+                    )))
+                } else {
+                    Err(anyhow!("basic auth not in the form username:password"))
+                }
             } else {
-                Err(anyhow!("basic auth not in the form username:password"))
+                Err(anyhow!("invalid base64 encoding"))
             }
         } else {
-            Err(anyhow!("invalid base64 encoding"))
+            Ok(None)
         }
     }
 }
 
 pub fn read_docker_config_json_file(path: &Path) -> Result<DockerConfig> {
     serde_json::from_reader::<_, DockerConfigRaw>(File::open(path)?)?.try_into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::iter::FromIterator;
+
+    #[test]
+    fn test_missing_auth_key() -> Result<()> {
+        let auths = vec![
+            (
+                "auth-registry.example.com".to_string(),
+                RegistryAuthRaw {
+                    // echo -n "username:password" | base64 -w0
+                    auth: Some("dXNlcm5hbWU6cGFzc3dvcmQ=".to_string()),
+                },
+            ),
+            (
+                "authless-registry.example.com".to_string(),
+                RegistryAuthRaw { auth: None },
+            ),
+        ];
+
+        let docker_config: DockerConfig = DockerConfigRaw {
+            auths: HashMap::from_iter(auths),
+        }
+        .try_into()?;
+
+        assert_eq!(docker_config.auths.len(), 1);
+
+        Ok(())
+    }
 }
