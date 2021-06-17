ac1263ef8f9bad3dca64531c43f2dafc716e55f6
Rafael Fernández López <ereslibre@ereslibre.es>
2021-06-17 10:56:42 +0200

    Drop ClusterAdmissionPolicy v1alpha1 version

--- controllers/policies/suite_test.go
+++ controllers/policies/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	policiesv1alpha1 "github.com/kubewarden/kubewarden-controller/apis/policies/v1alpha1"
+	policiesv1alpha2 "github.com/kubewarden/kubewarden-controller/apis/policies/v1alpha2"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -62,7 +62,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = policiesv1alpha1.AddToScheme(scheme.Scheme)
+	err = policiesv1alpha2.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
