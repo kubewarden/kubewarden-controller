40d4f5dc4dc400d46622896f0cbc9301fa6b58df
Flavio Castelli <fcastelli@suse.com>
2021-07-12 15:01:22 +0200

    code refactoring: slim `main.rs`
    Move the majority of its functions to the `cli.rs` file

--- crates/policy-server/src/cli.rs
+++ crates/policy-server/src/cli.rs
@@ -1,6 +1,13 @@
+use crate::settings::{read_policies_file, Policy};
+use anyhow::{anyhow, Result};
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+use policy_fetcher::registry::config::{read_docker_config_json_file, DockerConfig};
+use policy_fetcher::sources::{read_sources_file, Sources};
+use std::{collections::HashMap, net::SocketAddr, path::Path};
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
 
-pub fn build_cli() -> App<'static, 'static> {
+pub(crate) fn build_cli() -> App<'static, 'static> {
     App::new(crate_name!())
         .author(crate_authors!())
         .version(crate_version!())
@@ -86,4 +93,124 @@ pub fn build_cli() -> App<'static, 'static> {
                 .takes_value(true)
                 .help("Path to a Docker config.json-like path. Can be used to indicate registry authentication details"),
         )
+}
+
+pub(crate) fn api_bind_address(matches: &clap::ArgMatches) -> Result<SocketAddr> {
+    format!(
+        "{}:{}",
+        matches.value_of("address").unwrap(),
+        matches.value_of("port").unwrap()
+    )
+    .parse()
+    .map_err(|e| anyhow!("error parsing arguments: {}", e))
+}
+
+pub(crate) fn tls_files(matches: &clap::ArgMatches) -> Result<(String, String)> {
+    let cert_file = String::from(matches.value_of("cert-file").unwrap());
+    let key_file = String::from(matches.value_of("key-file").unwrap());
+    if cert_file.is_empty() != key_file.is_empty() {
+        Err(anyhow!("error parsing arguments: either both --cert-file and --key-file must be provided, or neither"))
+    } else {
+        Ok((cert_file, key_file))
+    }
+}
+
+pub(crate) fn policies(matches: &clap::ArgMatches) -> Result<HashMap<String, Policy>> {
+    let policies_file = Path::new(matches.value_of("policies").unwrap_or("."));
+    read_policies_file(policies_file).map_err(|e| {
+        anyhow!(
+            "error while loading policies from {:?}: {}",
+            policies_file,
+            e
+        )
+    })
+}
+
+// Setup the tracing system. This MUST be done inside of a tokio Runtime
+// because some collectors rely on it and would panic otherwise.
+pub(crate) fn setup_tracing(matches: &clap::ArgMatches) -> Result<()> {
+    // setup logging
+    let filter_layer = EnvFilter::new(matches.value_of("log-level").unwrap_or_default())
+        // some of our dependencies generate trace events too, but we don't care about them ->
+        // let's filter them
+        .add_directive("cranelift_codegen=off".parse().unwrap())
+        .add_directive("cranelift_wasm=off".parse().unwrap())
+        .add_directive("regalloc=off".parse().unwrap())
+        .add_directive("hyper::proto=off".parse().unwrap());
+
+    match matches.value_of("log-fmt").unwrap_or_default() {
+        "json" => tracing_subscriber::registry()
+            .with(filter_layer)
+            .with(fmt::layer().json())
+            .init(),
+        "text" => tracing_subscriber::registry()
+            .with(filter_layer)
+            .with(fmt::layer())
+            .init(),
+        "jaeger" => {
+            // Create a new OpenTelemetry pipeline sending events
+            // to a jaeger instance
+            // The Jaeger exporter can be configerd via environment
+            // variables (https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#jaeger-exporter)
+            let tracer = opentelemetry_jaeger::new_pipeline()
+                .with_service_name("kubewarden-policy-server")
+                .install_batch(opentelemetry::runtime::Tokio)?;
+
+            // Create a tracing layer with the configured tracer
+            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+            tracing_subscriber::registry()
+                .with(filter_layer)
+                .with(telemetry)
+                .with(fmt::layer())
+                .init()
+        }
+        "otlp" => {
+            // Create a new OpenTelemetry pipeline sending events to a
+            // OpenTelemetry collector using the OTLP format.
+            // The collector must run on localhost (eg: use a sidecar inside of k8s)
+            // using GRPC
+            let tracer = opentelemetry_otlp::new_pipeline()
+                .with_tonic()
+                .install_simple()?;
+
+            // Create a tracing layer with the configured tracer
+            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+            tracing_subscriber::registry()
+                .with(filter_layer)
+                .with(telemetry)
+                .with(fmt::layer())
+                .init()
+        }
+
+        _ => return Err(anyhow!("Unknown log message format")),
+    };
+
+    Ok(())
+}
+
+pub(crate) fn remote_server_options(
+    matches: &clap::ArgMatches,
+) -> Result<(Option<Sources>, Option<DockerConfig>)> {
+    let sources = match matches.value_of("sources-path") {
+        Some(sources_file) => Some(
+            read_sources_file(Path::new(sources_file))
+                .map_err(|e| anyhow!("error while loading sources from {}: {}", sources_file, e))?,
+        ),
+        None => None,
+    };
+
+    let docker_config = match matches.value_of("docker-config-json-path") {
+        Some(docker_config_json_path_file) => Some(
+            read_docker_config_json_file(Path::new(docker_config_json_path_file)).map_err(|e| {
+                anyhow!(
+                    "error while loading docker-config-json-like path from {}: {}",
+                    docker_config_json_path_file,
+                    e
+                )
+            })?,
+        ),
+        None => None,
+    };
+
+    Ok((sources, docker_config))
 }
--- crates/policy-server/src/main.rs
+++ crates/policy-server/src/main.rs
@@ -1,13 +1,11 @@
 extern crate k8s_openapi;
 extern crate kube;
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use opentelemetry::global::shutdown_tracer_provider;
-use std::{collections::HashMap, net::SocketAddr, process, thread};
+use std::{process, thread};
 use tokio::{runtime::Runtime, sync::mpsc, sync::oneshot};
 use tracing::{debug, error, info};
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::{fmt, EnvFilter};
 
 mod admission_review;
 mod api;
@@ -21,24 +19,19 @@ mod worker;
 mod worker_pool;
 use worker_pool::WorkerPool;
 
-use policy_fetcher::registry::config::{read_docker_config_json_file, DockerConfig};
-use policy_fetcher::sources::{read_sources_file, Sources};
-
-use settings::{read_policies_file, Policy};
-
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 mod communication;
 use communication::{EvalRequest, KubePollerBootRequest, WorkerPoolBootRequest};
 
-fn main() {
+fn main() -> Result<()> {
     let matches = cli::build_cli().get_matches();
 
     // init some variables based on the cli parameters
-    let addr = api_bind_address(&matches);
-    let (cert_file, key_file) = tls_files(&matches);
-    let mut policies = policies(&matches);
-    let (sources, docker_config) = remote_server_options(&matches);
+    let addr = cli::api_bind_address(&matches)?;
+    let (cert_file, key_file) = cli::tls_files(&matches)?;
+    let mut policies = cli::policies(&matches)?;
+    let (sources, docker_config) = cli::remote_server_options(&matches)?;
     let pool_size = matches.value_of("workers").map_or_else(num_cpus::get, |v| {
         v.parse::<usize>()
             .expect("error parsing the number of workers")
@@ -120,7 +113,7 @@ fn main() {
     rt.block_on(async {
         // Setup the tracing system. This MUST be done inside of a tokio Runtime
         // because some collectors rely on it and would panic otherwise.
-        if let Err(err) = setup_tracing(&matches) {
+        if let Err(err) = cli::setup_tracing(&matches) {
             fatal_error(err.to_string());
         }
 
@@ -250,140 +243,8 @@ fn main() {
     if let Err(e) = kube_poller_thread.join() {
         fatal_error(format!("error while waiting for worker threads: {:?}", e));
     };
-}
-
-// Setup the tracing system. This MUST be done inside of a tokio Runtime
-// because some collectors rely on it and would panic otherwise.
-fn setup_tracing(matches: &clap::ArgMatches) -> Result<()> {
-    // setup logging
-    let filter_layer = EnvFilter::new(matches.value_of("log-level").unwrap_or_default())
-        // some of our dependencies generate trace events too, but we don't care about them ->
-        // let's filter them
-        .add_directive("cranelift_codegen=off".parse().unwrap())
-        .add_directive("cranelift_wasm=off".parse().unwrap())
-        .add_directive("regalloc=off".parse().unwrap())
-        .add_directive("hyper::proto=off".parse().unwrap());
-
-    match matches.value_of("log-fmt").unwrap_or_default() {
-        "json" => tracing_subscriber::registry()
-            .with(filter_layer)
-            .with(fmt::layer().json())
-            .init(),
-        "text" => tracing_subscriber::registry()
-            .with(filter_layer)
-            .with(fmt::layer())
-            .init(),
-        "jaeger" => {
-            // Create a new OpenTelemetry pipeline sending events
-            // to a jaeger instance
-            // The Jaeger exporter can be configerd via environment
-            // variables (https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/sdk-environment-variables.md#jaeger-exporter)
-            let tracer = opentelemetry_jaeger::new_pipeline()
-                .with_service_name("kubewarden-policy-server")
-                .install_batch(opentelemetry::runtime::Tokio)?;
-
-            // Create a tracing layer with the configured tracer
-            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-            tracing_subscriber::registry()
-                .with(filter_layer)
-                .with(telemetry)
-                .with(fmt::layer())
-                .init()
-        }
-        "otlp" => {
-            // Create a new OpenTelemetry pipeline sending events to a
-            // OpenTelemetry collector using the OTLP format.
-            // The collector must run on localhost (eg: use a sidecar inside of k8s)
-            // using GRPC
-            let tracer = opentelemetry_otlp::new_pipeline()
-                .with_tonic()
-                .install_simple()?;
-
-            // Create a tracing layer with the configured tracer
-            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-            tracing_subscriber::registry()
-                .with(filter_layer)
-                .with(telemetry)
-                .with(fmt::layer())
-                .init()
-        }
-
-        _ => return Err(anyhow!("Unknown log message format")),
-    };
 
     Ok(())
-}
-
-fn remote_server_options(matches: &clap::ArgMatches) -> (Option<Sources>, Option<DockerConfig>) {
-    let sources = matches.value_of("sources-path").map(|sources_file| {
-        match read_sources_file(Path::new(sources_file)) {
-            Ok(sources) => sources,
-            Err(err) => {
-                fatal_error(format!(
-                    "error while loading sources from {}: {}",
-                    sources_file, err
-                ));
-                unreachable!();
-            }
-        }
-    });
-
-    let docker_config =
-        matches
-            .value_of("docker-config-json-path")
-            .map(|docker_config_json_path_file| {
-                match read_docker_config_json_file(Path::new(docker_config_json_path_file)) {
-                    Ok(docker_config_json) => docker_config_json,
-                    Err(err) => {
-                        fatal_error(format!(
-                            "error while loading docker-config-json-like path from {}: {}",
-                            docker_config_json_path_file, err
-                        ));
-                        unreachable!();
-                    }
-                }
-            });
-
-    (sources, docker_config)
-}
-
-fn api_bind_address(matches: &clap::ArgMatches) -> SocketAddr {
-    match format!(
-        "{}:{}",
-        matches.value_of("address").unwrap(),
-        matches.value_of("port").unwrap()
-    )
-    .parse()
-    {
-        Ok(a) => a,
-        Err(error) => {
-            fatal_error(format!("error parsing arguments: {}", error));
-            unreachable!();
-        }
-    }
-}
-
-fn tls_files(matches: &clap::ArgMatches) -> (String, String) {
-    let cert_file = String::from(matches.value_of("cert-file").unwrap());
-    let key_file = String::from(matches.value_of("key-file").unwrap());
-    if cert_file.is_empty() != key_file.is_empty() {
-        fatal_error("error parsing arguments: either both --cert-file and --key-file must be provided, or neither.".to_string());
-    };
-    (cert_file, key_file)
-}
-
-fn policies(matches: &clap::ArgMatches) -> HashMap<String, Policy> {
-    let policies_file = Path::new(matches.value_of("policies").unwrap_or("."));
-    match read_policies_file(policies_file) {
-        Ok(policies) => policies,
-        Err(err) => {
-            fatal_error(format!(
-                "error while loading policies from {:?}: {}",
-                policies_file, err
-            ));
-            unreachable!();
-        }
-    }
 }
 
 fn fatal_error(msg: String) {
