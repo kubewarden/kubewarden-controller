979f8cbac9dfa545979fc01a400b64d6cb5128e3
Flavio Castelli <fcastelli@suse.com>
2021-08-10 10:40:44 +0200

    Remove commented code
    Get rid of some leftovers

--- crates/policy-evaluator/crates/burrego/src/opa/builtins/mod.rs
+++ crates/policy-evaluator/crates/burrego/src/opa/builtins/mod.rs
@@ -7,10 +7,6 @@ pub type BuiltinFunctionsMap =
     HashMap<&'static str, fn(&[serde_json::Value]) -> Result<serde_json::Value>>;
 
 pub fn get_builtins() -> BuiltinFunctionsMap {
-    //let mut functions: HashMap<
-    //    &'static str,
-    //    fn(&Vec<serde_json::Value>) -> Result<serde_json::Value>,
-    //> = HashMap::new();
     let mut functions: BuiltinFunctionsMap = HashMap::new();
     functions.insert("sprintf", strings::sprintf);
 
