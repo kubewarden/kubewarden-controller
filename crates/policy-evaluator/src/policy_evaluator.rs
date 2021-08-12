5f9a834167e4fc71b931846b310faa6c287d9d62
Rafael Fernández López <ereslibre@ereslibre.es>
2021-08-12 11:38:46 +0200

    refactor: remove big matches on enum at validatio
    This refactors the internal structure such that:
    
    - Keep the `Runtime` enum that tells the evaluator what runtimes shoud
    be used to evaluate a policy.
    
    - Add a new `RuntimeBackend` trait that has three functions:
    `validate`, `validate_settings` and `protocol_version`. This is the
    trait that the backends implement.
    
    - Include two unit structures: `WapcRuntime` and `BurregoRuntime` that
    helps us to separate logic, and lets us implement the shared
    `RuntimeBackend` trait.

--- crates/policy-evaluator/src/policy_evaluator.rs
+++ crates/policy-evaluator/src/policy_evaluator.rs
@@ -3,7 +3,7 @@ use lazy_static::lazy_static;
 use serde::Serialize;
 use serde_json::{json, value};
 use std::{collections::HashMap, convert::TryFrom, fmt, fs, path::Path, sync::RwLock};
-use tracing::{debug, error};
+use tracing::error;
 
 use wapc::WapcHost;
 use wasmtime_provider::WasmtimeEngineProvider;
@@ -97,24 +97,222 @@ pub(crate) fn host_callback(
     }
 }
 
-pub struct BurregoEvaluator {
+struct BurregoEvaluator {
     evaluator: burrego::opa::wasm::Evaluator,
     entrypoint_id: i32,
     input: serde_json::Value,
     data: serde_json::Value,
 }
 
-pub enum Runtime {
+enum Runtime {
     Wapc(wapc::WapcHost),
     // The `BurregoEvaluator` variant is boxed since it outsizes the
     // other variants of this enum.
     Burrego(Box<BurregoEvaluator>),
 }
 
+struct WapcRuntime<'a>(&'a mut wapc::WapcHost);
+struct BurregoRuntime<'a>(&'a mut BurregoEvaluator);
+
+type PolicySettings = serde_json::Map<String, serde_json::Value>;
+
+trait RuntimeBackend {
+    fn validate(
+        &mut self,
+        settings: &PolicySettings,
+        request: &ValidateRequest,
+    ) -> ValidationResponse;
+    fn validate_settings(&mut self, settings: String) -> SettingsValidationResponse;
+    fn protocol_version(&self) -> Result<ProtocolVersion> {
+        Err(anyhow!(
+            "protocol_version is not applicable to this policy runtime"
+        ))
+    }
+}
+
+impl<'a> RuntimeBackend for WapcRuntime<'a> {
+    fn validate(
+        &mut self,
+        settings: &PolicySettings,
+        request: &ValidateRequest,
+    ) -> ValidationResponse {
+        let uid = request.uid();
+
+        let req_obj = match request.0.get("object") {
+            Some(req_obj) => req_obj,
+            None => {
+                return ValidationResponse::reject(
+                    uid.to_string(),
+                    "request doesn't have an 'object' value".to_string(),
+                    hyper::StatusCode::BAD_REQUEST.as_u16(),
+                );
+            }
+        };
+
+        let validate_params = json!({
+            "request": request,
+            "settings": settings,
+        });
+
+        let validate_str = match serde_json::to_string(&validate_params) {
+            Ok(s) => s,
+            Err(e) => {
+                error!(
+                    error = e.to_string().as_str(),
+                    "cannot serialize validation params"
+                );
+                return ValidationResponse::reject_internal_server_error(
+                    uid.to_string(),
+                    e.to_string(),
+                );
+            }
+        };
+
+        match self.0.call("validate", validate_str.as_bytes()) {
+            Ok(res) => {
+                let pol_val_resp: Result<PolicyValidationResponse> = serde_json::from_slice(&res)
+                    .map_err(|e| anyhow!("cannot deserialize policy validation response: {:?}", e));
+                pol_val_resp
+                    .and_then(|pol_val_resp| {
+                        ValidationResponse::from_policy_validation_response(
+                            uid.to_string(),
+                            req_obj,
+                            &pol_val_resp,
+                        )
+                    })
+                    .unwrap_or_else(|e| {
+                        error!(
+                            error = e.to_string().as_str(),
+                            "cannot build validation response from policy result"
+                        );
+                        ValidationResponse::reject_internal_server_error(
+                            uid.to_string(),
+                            e.to_string(),
+                        )
+                    })
+            }
+            Err(e) => {
+                error!(error = e.to_string().as_str(), "waPC communication error");
+                ValidationResponse::reject_internal_server_error(uid.to_string(), e.to_string())
+            }
+        }
+    }
+
+    fn validate_settings(&mut self, settings: String) -> SettingsValidationResponse {
+        match self.0.call("validate_settings", settings.as_bytes()) {
+            Ok(res) => {
+                let vr: Result<SettingsValidationResponse> = serde_json::from_slice(&res)
+                    .map_err(|e| anyhow!("cannot convert response: {:?}", e));
+                vr.unwrap_or_else(|e| SettingsValidationResponse {
+                    valid: false,
+                    message: Some(format!("error: {:?}", e)),
+                })
+            }
+            Err(err) => SettingsValidationResponse {
+                valid: false,
+                message: Some(format!(
+                    "Error invoking settings validation callback: {:?}",
+                    err
+                )),
+            },
+        }
+    }
+
+    fn protocol_version(&self) -> Result<ProtocolVersion> {
+        match self.0.call("protocol_version", &[0; 0]) {
+            Ok(res) => ProtocolVersion::try_from(res.clone()).map_err(|e| {
+                anyhow!(
+                    "Cannot create ProtocolVersion object from '{:?}': {:?}",
+                    res,
+                    e
+                )
+            }),
+            Err(err) => Err(anyhow!(
+                "Cannot invoke 'protocol_version' waPC function: {:?}",
+                err
+            )),
+        }
+    }
+}
+
+impl<'a> RuntimeBackend for BurregoRuntime<'a> {
+    fn validate(
+        &mut self,
+        _settings: &PolicySettings,
+        request: &ValidateRequest,
+    ) -> ValidationResponse {
+        let uid = request.uid();
+
+        let burrego_evaluation =
+            self.0
+                .evaluator
+                .evaluate(self.0.entrypoint_id, &self.0.input, &self.0.data);
+
+        match burrego_evaluation {
+            Ok(evaluation_result) => {
+                // According to the OPA WASM
+                // documentation/specification, the result of
+                // the evaluation will contain an object of
+                // the form:
+                //
+                // ```
+                // [
+                //   {
+                //     "result": "<the AdmissionReview object -- generated by the policy>"
+                //   }
+                // ]
+                // ```
+                //
+                // Reference: https://www.openpolicyagent.org/docs/v0.31.0/wasm/#compiling-policies
+                let evaluation_result = evaluation_result
+                    .get(0)
+                    .and_then(|r| r.get("result"))
+                    .and_then(|r| r.get("response"));
+
+                match evaluation_result {
+                    Some(evaluation_result) => {
+                        match serde_json::from_value(evaluation_result.clone()) {
+                            Ok(evaluation_result) => ValidationResponse {
+                                uid: uid.to_string(),
+                                ..evaluation_result
+                            },
+                            Err(err) => ValidationResponse::reject_internal_server_error(
+                                uid.to_string(),
+                                err.to_string(),
+                            ),
+                        }
+                    }
+                    None => ValidationResponse::reject_internal_server_error(
+                        uid.to_string(),
+                        "cannot interpret OPA policy result".to_string(),
+                    ),
+                }
+            }
+            Err(err) => {
+                error!(
+                    error = err.to_string().as_str(),
+                    "error evaluating policy with burrego"
+                );
+                ValidationResponse::reject_internal_server_error(uid.to_string(), err.to_string())
+            }
+        }
+    }
+
+    fn validate_settings(&mut self, _settings: String) -> SettingsValidationResponse {
+        // The burrego backend is mainly for compatibility with
+        // existing OPA policies. Those policies don't have a generic
+        // way of validating settings. Return true
+        SettingsValidationResponse {
+            valid: true,
+            message: None,
+        }
+    }
+}
+
 pub struct PolicyEvaluator {
     runtime: Runtime,
     policy: Policy,
-    settings: serde_json::Map<String, serde_json::Value>,
+    settings: PolicySettings,
 }
 
 impl fmt::Debug for PolicyEvaluator {
@@ -203,184 +401,41 @@ impl PolicyEvaluator {
 
     #[tracing::instrument(skip(request))]
     pub fn validate(&mut self, request: ValidateRequest) -> ValidationResponse {
-        let uid = request.uid();
-
-        let req_obj = match request.0.get("object") {
-            Some(req_obj) => req_obj,
-            None => {
-                return ValidationResponse::reject(
-                    uid.to_string(),
-                    "request doesn't have an 'object' value".to_string(),
-                    hyper::StatusCode::BAD_REQUEST.as_u16(),
-                );
+        match self.runtime {
+            Runtime::Wapc(ref mut wapc_host) => {
+                WapcRuntime(wapc_host).validate(&self.settings, &request)
             }
-        };
-        let validate_params = json!({
-            "request": request,
-            "settings": self.settings,
-        });
-        let validate_str = match serde_json::to_string(&validate_params) {
-            Ok(s) => s,
-            Err(e) => {
-                error!(
-                    error = e.to_string().as_str(),
-                    "cannot serialize validation params"
-                );
-                return ValidationResponse::reject_internal_server_error(
-                    uid.to_string(),
-                    e.to_string(),
-                );
-            }
-        };
-        match &mut self.runtime {
-            Runtime::Wapc(wapc_host) => match wapc_host.call("validate", validate_str.as_bytes()) {
-                Ok(res) => {
-                    let pol_val_resp: Result<PolicyValidationResponse> =
-                        serde_json::from_slice(&res).map_err(|e| {
-                            anyhow!("cannot deserialize policy validation response: {:?}", e)
-                        });
-                    pol_val_resp
-                        .and_then(|pol_val_resp| {
-                            ValidationResponse::from_policy_validation_response(
-                                uid.to_string(),
-                                req_obj,
-                                &pol_val_resp,
-                            )
-                        })
-                        .unwrap_or_else(|e| {
-                            error!(
-                                error = e.to_string().as_str(),
-                                "cannot build validation response from policy result"
-                            );
-                            ValidationResponse::reject_internal_server_error(
-                                uid.to_string(),
-                                e.to_string(),
-                            )
-                        })
-                }
-                Err(e) => {
-                    error!(error = e.to_string().as_str(), "waPC communication error");
-                    ValidationResponse::reject_internal_server_error(uid.to_string(), e.to_string())
-                }
-            },
-            Runtime::Burrego(ref mut burrego) => {
-                let burrego_evaluation = burrego.evaluator.evaluate(
-                    burrego.entrypoint_id,
-                    &burrego.input,
-                    &burrego.data,
-                );
-                match burrego_evaluation {
-                    Ok(evaluation_result) => {
-                        // According to the OPA WASM
-                        // documentation/specification, the result of
-                        // the evaluation will contain an object of
-                        // the form:
-                        //
-                        // ```
-                        // [
-                        //   {
-                        //     "result": "<the AdmissionReview object -- generated by the policy>"
-                        //   }
-                        // ]
-                        // ```
-                        //
-                        // Reference: https://www.openpolicyagent.org/docs/v0.31.0/wasm/#compiling-policies
-                        let evaluation_result = evaluation_result
-                            .get(0)
-                            .and_then(|r| r.get("result"))
-                            .and_then(|r| r.get("response"));
-
-                        match evaluation_result {
-                            Some(evaluation_result) => {
-                                match serde_json::from_value(evaluation_result.clone()) {
-                                    Ok(evaluation_result) => ValidationResponse {
-                                        uid: uid.to_string(),
-                                        ..evaluation_result
-                                    },
-                                    Err(err) => ValidationResponse::reject_internal_server_error(
-                                        uid.to_string(),
-                                        err.to_string(),
-                                    ),
-                                }
-                            }
-                            None => ValidationResponse::reject_internal_server_error(
-                                uid.to_string(),
-                                "cannot interpret OPA policy result".to_string(),
-                            ),
-                        }
-                    }
-                    Err(err) => {
-                        error!(
-                            error = err.to_string().as_str(),
-                            "error evaluating policy with burrego"
-                        );
-                        ValidationResponse::reject_internal_server_error(
-                            uid.to_string(),
-                            err.to_string(),
-                        )
-                    }
-                }
+            Runtime::Burrego(ref mut burrego_evaluator) => {
+                BurregoRuntime(burrego_evaluator).validate(&self.settings, &request)
             }
         }
     }
 
     #[tracing::instrument]
-    pub fn validate_settings(&self) -> SettingsValidationResponse {
+    pub fn validate_settings(&mut self) -> SettingsValidationResponse {
         let settings_str = match serde_json::to_string(&self.settings) {
-            Ok(s) => s,
-            Err(e) => {
+            Ok(settings) => settings,
+            Err(err) => {
                 return SettingsValidationResponse {
                     valid: false,
-                    message: Some(format!("Cannot serialize validation params: {}", e)),
+                    message: Some(format!("could not marshal setings: {}", err)),
                 }
             }
         };
 
-        match &self.runtime {
-            Runtime::Wapc(wapc_host) => {
-                match wapc_host.call("validate_settings", settings_str.as_bytes()) {
-                    Ok(res) => {
-                        let vr: Result<SettingsValidationResponse> = serde_json::from_slice(&res)
-                            .map_err(|e| anyhow!("cannot convert response: {:?}", e));
-                        vr.unwrap_or_else(|e| SettingsValidationResponse {
-                            valid: false,
-                            message: Some(format!("error: {:?}", e)),
-                        })
-                    }
-                    Err(err) => SettingsValidationResponse {
-                        valid: false,
-                        message: Some(format!(
-                            "Error invoking settings validation callback: {:?}",
-                            err
-                        )),
-                    },
-                }
+        match self.runtime {
+            Runtime::Wapc(ref mut wapc_host) => {
+                WapcRuntime(wapc_host).validate_settings(settings_str)
             }
-            Runtime::Burrego(_) => {
-                debug!("settings cannot be evaluated by a burrego runtime");
-                SettingsValidationResponse {
-                    valid: true,
-                    message: None,
-                }
+            Runtime::Burrego(ref mut burrego_evaluator) => {
+                BurregoRuntime(burrego_evaluator).validate_settings(settings_str)
             }
         }
     }
 
-    pub fn protocol_version(&self) -> Result<ProtocolVersion> {
-        match &self.runtime {
-            Runtime::Wapc(wapc_host) => match wapc_host.call("protocol_version", &[0; 0]) {
-                Ok(res) => ProtocolVersion::try_from(res.clone()).map_err(|e| {
-                    anyhow!(
-                        "Cannot create ProtocolVersion object from '{:?}': {:?}",
-                        res,
-                        e
-                    )
-                }),
-                Err(err) => Err(anyhow!(
-                    "Cannot invoke 'protocol_version' waPC function: {:?}",
-                    err
-                )),
-            },
+    pub fn protocol_version(&mut self) -> Result<ProtocolVersion> {
+        match &mut self.runtime {
+            Runtime::Wapc(ref mut wapc_host) => WapcRuntime(wapc_host).protocol_version(),
             _ => Err(anyhow!(
                 "protocol_version is only applicable to a Kubewarden policy"
             )),
