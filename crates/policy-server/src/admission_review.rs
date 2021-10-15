9b769524f6728990d27b79cd404c9a9588df9b5d
Rafael Fernández López <rfernandezlopez@suse.com>
2021-10-16 00:15:10 +0200

    Skip serializing optional fields
    Instead of reporting them as null, skip them altogether. This is akin
    to the `omitempty` behavior of the go serializing logic.

--- crates/policy-server/src/admission_review.rs
+++ crates/policy-server/src/admission_review.rs
@@ -20,17 +20,27 @@ pub(crate) struct AdmissionRequest {
     pub uid: String,
     pub kind: GroupVersionKind,
     pub resource: GroupVersionResource,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub sub_resource: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub request_kind: Option<GroupVersionKind>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub request_resource: Option<GroupVersionResource>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub request_sub_resource: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub namespace: Option<String>,
     pub operation: String,
     pub user_info: k8s_openapi::api::authentication::v1::UserInfo,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub object: Option<k8s_openapi::apimachinery::pkg::runtime::RawExtension>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub old_object: Option<k8s_openapi::apimachinery::pkg::runtime::RawExtension>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub dry_run: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub options: Option<k8s_openapi::apimachinery::pkg::runtime::RawExtension>,
 }
 
