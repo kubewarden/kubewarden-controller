18fdf72ef10c774e30731c0e21d48cc60c336c8b
José Guilherme Vanz <jguilhermevanz@suse.com>
2021-10-19 09:54:24 -0300

    Update verbs used by the default webhook function.
    To keep the webhook configuration consistent to the code which register
    the webhook the verbs configured to the default function has been update
    adding the "update" verb.

--- apis/policies/v1alpha2/policyserver_webhook.go
+++ apis/policies/v1alpha2/policyserver_webhook.go
@@ -39,7 +39,7 @@ func (ps *PolicyServer) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return nil
 }
 
-//+kubebuilder:webhook:path=/mutate-policies-kubewarden-io-v1alpha2-policyserver,mutating=true,failurePolicy=fail,sideEffects=None,groups=policies.kubewarden.io,resources=policyservers,verbs=create,versions=v1alpha2,name=mpolicyserver.kb.io,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/mutate-policies-kubewarden-io-v1alpha2-policyserver,mutating=true,failurePolicy=fail,sideEffects=None,groups=policies.kubewarden.io,resources=policyservers,verbs=create;update,versions=v1alpha2,name=mpolicyserver.kb.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Defaulter = &PolicyServer{}
 
