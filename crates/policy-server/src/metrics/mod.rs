ereslibre/policy-server

Implement metrics infrastructure and first metrics
Implement metrics infrastructure by instrumenting the policy-server
with opentelemetry.

The first metric is the total policy evaluations.

src/metrics/mod.rs
@@ -1,0 +1,19 @@
+use opentelemetry::metrics;
+use opentelemetry::sdk::metrics::PushController;
+use opentelemetry_otlp::{ExportConfig, WithExportConfig};
+
+mod policy_evaluations_total;
+pub use policy_evaluations_total::{observe_policy_evaluation, PolicyEvaluation};
+
+const METER_NAME: &str = "kubewarden";
+
+pub(crate) fn init_meter() -> metrics::Result<PushController> {
+    opentelemetry_otlp::new_pipeline()
+        .metrics(tokio::spawn, opentelemetry::util::tokio_interval_stream)
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_export_config(ExportConfig::default()),
+        )
+        .build()
+}

src/metrics/policy_evaluations_total.rs
@@ -1,0 +1,53 @@
+use lazy_static::lazy_static;
+use opentelemetry::{
+    metrics::{self, Counter, Meter},
+    KeyValue,
+};
+
+lazy_static! {
+    static ref POLICY_EVALUATIONS_TOTAL: Counter<u64> =
+        opentelemetry::global::meter(super::METER_NAME)
+            .u64_counter("kubewarden_policy_evaluations_total")
+            .init();
+}
+
+pub struct PolicyEvaluation {
+    pub(crate) policy_name: String,
+    pub(crate) resource_name: String,
+    pub(crate) resource_kind: String,
+    pub(crate) resource_namespace: Option<String>,
+    pub(crate) resource_request_operation: String,
+    pub(crate) accepted: bool,
+    pub(crate) mutated: bool,
+    pub(crate) error_code: Option<u16>,
+}
+
+impl Into<Vec<KeyValue>> for PolicyEvaluation {
+    fn into(self) -> Vec<KeyValue> {
+        let mut baggage = vec![
+            KeyValue::new("policy_name", self.policy_name),
+            KeyValue::new("resource_name", self.resource_name),
+            KeyValue::new("resource_kind", self.resource_kind),
+            KeyValue::new(
+                "resource_request_operation",
+                self.resource_request_operation,
+            ),
+            KeyValue::new("accepted", self.accepted),
+            KeyValue::new("mutated", self.mutated),
+        ];
+        if let Some(resource_namespace) = self.resource_namespace {
+            baggage.append(&mut vec![KeyValue::new(
+                "resource_namespace",
+                resource_namespace,
+            )]);
+        }
+        if let Some(error_code) = self.error_code {
+            baggage.append(&mut vec![KeyValue::new("error_code", error_code as i64)]);
+        }
+        baggage
+    }
+}
+
+pub fn observe_policy_evaluation(policy_evaluation: PolicyEvaluation) {
+    POLICY_EVALUATIONS_TOTAL.add(1, &Into::<Vec<KeyValue>>::into(policy_evaluation));
+}

src/worker.rs
@@ -1,4 +1,5 @@
 use crate::communication::EvalRequest;
+use crate::metrics;
 use crate::settings::Policy;
 use crate::utils::convert_yaml_map_to_json;
 use anyhow::Result;
@@ -10,6 +11,7 @@ use policy_evaluator::{
 };
 use std::collections::HashMap;
 use std::fmt;
+use std::iter::FilterMap;
 use tokio::sync::mpsc::Receiver;
 use tracing::{error, info_span};
 
@@ -130,10 +132,28 @@ impl Worker {
             let _enter = span.enter();
 
             let res = match self.evaluators.get_mut(&req.policy_id) {
-                Some(policy_evaluator) => match serde_json::to_value(req.req) {
+                Some(policy_evaluator) => match serde_json::to_value(req.req.clone()) {
                     Ok(json) => {
                         let resp = policy_evaluator.validate(ValidateRequest::new(json));
-                        req.resp_chan.send(Some(resp))
+                        let accepted = resp.allowed;
+                        let mutated = resp.patch.is_some();
+                        let error_code = if let Some(status) = &resp.status {
+                            status.code
+                        } else {
+                            None
+                        };
+                        let res = req.resp_chan.send(Some(resp));
+                        metrics::observe_policy_evaluation(metrics::PolicyEvaluation {
+                            policy_name: policy_evaluator.policy.id.clone(),
+                            resource_name: req.req.name.unwrap_or_else(|| "unknown".to_string()),
+                            resource_namespace: req.req.namespace,
+                            resource_kind: req.req.request_kind.unwrap_or_default().kind,
+                            resource_request_operation: req.req.operation.clone(),
+                            accepted,
+                            mutated,
+                            error_code,
+                        });
+                        res
                     }
                     Err(e) => {
                         let error_msg = format!("Failed to serialize AdmissionReview: {:?}", e);

kubewarden/kubewarden-controller

crates/policy-server/src/metrics/mod.rs
@@ -1,0 +1,19 @@
+use opentelemetry::metrics;
+use opentelemetry::sdk::metrics::PushController;
+use opentelemetry_otlp::{ExportConfig, WithExportConfig};
+
+mod policy_evaluations_total;
+pub use policy_evaluations_total::{add_policy_evaluation, PolicyEvaluation};
+
+const METER_NAME: &str = "kubewarden";
+
+pub(crate) fn init_meter() -> metrics::Result<PushController> {
+    opentelemetry_otlp::new_pipeline()
+        .metrics(tokio::spawn, opentelemetry::util::tokio_interval_stream)
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_export_config(ExportConfig::default()),
+        )
+        .build()
+}

crates/policy-server/src/metrics/policy_evaluations_total.rs
@@ -1,0 +1,51 @@
+use lazy_static::lazy_static;
+use opentelemetry::{metrics::Counter, KeyValue};
+
+lazy_static! {
+    static ref POLICY_EVALUATIONS_TOTAL: Counter<u64> =
+        opentelemetry::global::meter(super::METER_NAME)
+            .u64_counter("kubewarden_policy_evaluations_total")
+            .init();
+}
+
+pub struct PolicyEvaluation {
+    pub(crate) policy_name: String,
+    pub(crate) resource_name: String,
+    pub(crate) resource_kind: String,
+    pub(crate) resource_namespace: Option<String>,
+    pub(crate) resource_request_operation: String,
+    pub(crate) accepted: bool,
+    pub(crate) mutated: bool,
+    pub(crate) error_code: Option<u16>,
+}
+
+#[allow(clippy::from_over_into)]
+impl Into<Vec<KeyValue>> for PolicyEvaluation {
+    fn into(self) -> Vec<KeyValue> {
+        let mut baggage = vec![
+            KeyValue::new("policy_name", self.policy_name),
+            KeyValue::new("resource_name", self.resource_name),
+            KeyValue::new("resource_kind", self.resource_kind),
+            KeyValue::new(
+                "resource_request_operation",
+                self.resource_request_operation,
+            ),
+            KeyValue::new("accepted", self.accepted),
+            KeyValue::new("mutated", self.mutated),
+        ];
+        if let Some(resource_namespace) = self.resource_namespace {
+            baggage.append(&mut vec![KeyValue::new(
+                "resource_namespace",
+                resource_namespace,
+            )]);
+        }
+        if let Some(error_code) = self.error_code {
+            baggage.append(&mut vec![KeyValue::new("error_code", error_code as i64)]);
+        }
+        baggage
+    }
+}
+
+pub fn add_policy_evaluation(policy_evaluation: PolicyEvaluation) {
+    POLICY_EVALUATIONS_TOTAL.add(1, &Into::<Vec<KeyValue>>::into(policy_evaluation));
+}

crates/policy-server/src/worker.rs
@@ -1,4 +1,5 @@
 use crate::communication::EvalRequest;
+use crate::metrics;
 use crate::settings::Policy;
 use crate::utils::convert_yaml_map_to_json;
 use anyhow::Result;
@@ -130,10 +131,28 @@ impl Worker {
             let _enter = span.enter();
 
             let res = match self.evaluators.get_mut(&req.policy_id) {
-                Some(policy_evaluator) => match serde_json::to_value(req.req) {
+                Some(policy_evaluator) => match serde_json::to_value(req.req.clone()) {
                     Ok(json) => {
                         let resp = policy_evaluator.validate(ValidateRequest::new(json));
-                        req.resp_chan.send(Some(resp))
+                        let accepted = resp.allowed;
+                        let mutated = resp.patch.is_some();
+                        let error_code = if let Some(status) = &resp.status {
+                            status.code
+                        } else {
+                            None
+                        };
+                        let res = req.resp_chan.send(Some(resp));
+                        metrics::add_policy_evaluation(metrics::PolicyEvaluation {
+                            policy_name: policy_evaluator.policy.id.clone(),
+                            resource_name: req.req.name.unwrap_or_else(|| "unknown".to_string()),
+                            resource_namespace: req.req.namespace,
+                            resource_kind: req.req.request_kind.unwrap_or_default().kind,
+                            resource_request_operation: req.req.operation.clone(),
+                            accepted,
+                            mutated,
+                            error_code,
+                        });
+                        res
                     }
                     Err(e) => {
                         let error_msg = format!("Failed to serialize AdmissionReview: {:?}", e);