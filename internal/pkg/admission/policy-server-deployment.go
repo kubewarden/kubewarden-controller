e39e03c6aae354c97e46eda0c1bf326e9bc479cc
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-10-26 14:01:33 +0200

    feat: Add PolicyServer.spec.sourceAuthorities to CR
    This new field allows for passing PEM certificates per URI:
    
    ```
    spec:
      sourceAuthorities:
        "example.com:5000":
        - |
          pem cert 1
        - "pem cert 2"
    ```
    
    Such list of certificates per URI get converted to the expected format
    by policy-server's sources.yml, and added to the associated configmap of
    the PolicyServer, under `data.sources.yml`. Which then gets mounted as
    usual.

--- internal/pkg/admission/policy-server-deployment.go
+++ internal/pkg/admission/policy-server-deployment.go
@@ -182,7 +182,6 @@ func (r *Reconciler) deployment(configMapVersion string, policyServer *policiesv
 	const (
 		certsVolumeName                  = "certs"
 		policiesConfigContainerPath      = "/config"
-		sourcesConfigContainerPath       = "/sources"
 		policiesFilename                 = "policies.yml"
 		sourcesFilename                  = "sources.yml"
 		policiesVolumeName               = "policies"
@@ -254,7 +253,7 @@ func (r *Reconciler) deployment(configMapVersion string, policyServer *policiesv
 			},
 		)
 	}
-	if len(policyServer.Spec.InsecureSources) > 0 {
+	if len(policyServer.Spec.InsecureSources) > 0 || len(policyServer.Spec.SourceAuthorities) > 0 {
 		admissionContainer.VolumeMounts = append(admissionContainer.VolumeMounts,
 			corev1.VolumeMount{
 				Name:      sourcesVolumeName,
@@ -347,7 +346,7 @@ func (r *Reconciler) deployment(configMapVersion string, policyServer *policiesv
 			},
 		)
 	}
-	if len(policyServer.Spec.InsecureSources) > 0 {
+	if len(policyServer.Spec.InsecureSources) > 0 || len(policyServer.Spec.SourceAuthorities) > 0 {
 		policyServerDeployment.Spec.Template.Spec.Volumes = append(
 			policyServerDeployment.Spec.Template.Spec.Volumes,
 			corev1.Volume{
@@ -356,12 +355,6 @@ func (r *Reconciler) deployment(configMapVersion string, policyServer *policiesv
 					ConfigMap: &corev1.ConfigMapVolumeSource{
 						LocalObjectReference: corev1.LocalObjectReference{
 							Name: policyServer.NameWithPrefix(),
-						},
-						Items: []corev1.KeyToPath{
-							{
-								Key:  constants.PolicyServerConfigSourcesEntry,
-								Path: sourcesFilename,
-							},
 						},
 					},
 				},
