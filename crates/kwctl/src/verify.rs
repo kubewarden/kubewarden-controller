02a2236a4cf3024588581aa95599e5281722d584
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-11-12 11:22:12 +0100

    feat: Add `verify::verify_local_checksum()`
    Refactor `verify:verify()` to output a verified_manifest_digest. This is
    used by `verify::verify_local_checksum()`. That way, we make sure that
    users of `verify::verify_local_checksum()` are aware that they need to
    call it against an already verified policy.

--- crates/kwctl/src/verify.rs
+++ crates/kwctl/src/verify.rs
@@ -2,8 +2,8 @@ use anyhow::{anyhow, Result};
 use policy_fetcher::registry::config::DockerConfig;
 use policy_fetcher::sources::Sources;
 use policy_fetcher::verify::Verifier;
-use std::io::prelude::*;
-use std::{collections::HashMap, fs::File};
+use std::{collections::HashMap, fs};
+use tracing::info;
 
 pub(crate) async fn verify(
     url: &str,
@@ -11,19 +11,39 @@ pub(crate) async fn verify(
     sources: Option<Sources>,
     annotations: Option<HashMap<String, String>>,
     key_file: &str,
-) -> Result<()> {
-    let mut pub_key_file =
-        File::open(key_file).map_err(|e| anyhow!("Cannot read verification key file: {:?}", e))?;
-    let mut verification_key = String::new();
-    pub_key_file
-        .read_to_string(&mut verification_key)
-        .map_err(|e| anyhow!("Error reading contents of verification key file {:?}", e))?;
-
-    let mut verifier = Verifier::new(sources);
-    verifier
-        .verify(url, docker_config, annotations, &verification_key)
+) -> Result<String> {
+    let verification_key = read_key_file(key_file)?;
+    let mut verifier = Verifier::new(sources.clone());
+    let verified_manifest_digest = verifier
+        .verify(
+            url,
+            docker_config.clone(),
+            annotations.clone(),
+            &verification_key,
+        )
         .await?;
 
-    println!("Policy successfully verified");
+    info!("Policy successfully verified");
+    Ok(verified_manifest_digest)
+}
+
+pub(crate) async fn verify_local_checksum(
+    url: &str,
+    docker_config: &Option<DockerConfig>,
+    sources: &Option<Sources>,
+    verified_manifest_digest: &str,
+) -> Result<()> {
+    let mut verifier = Verifier::new(sources.clone());
+    verifier
+        .verify_local_file_checksum(url, docker_config.clone(), verified_manifest_digest)
+        .await?;
+
+    info!("Local checksum successfully verified");
     Ok(())
+}
+
+fn read_key_file(path: &str) -> Result<String> {
+    let verification_key =
+        fs::read_to_string(path).map_err(|e| anyhow!("Something went wrong: {:?}", e))?;
+    Ok(verification_key)
 }
