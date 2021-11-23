d57de7af2a1a0e110033a58314ee415037e1c68a
Flavio Castelli <fcastelli@suse.com>
2021-11-23 09:44:35 +0100

    Update src/callback_requests.rs
    Co-authored-by: Rafael Fernández López <ereslibre@kde.org>

--- crates/policy-evaluator/src/callback_requests.rs
+++ crates/policy-evaluator/src/callback_requests.rs
@@ -15,7 +15,7 @@ pub enum CallbackRequest {
     /// Require the computation of the manifest digest of an OCI object (be
     /// it an image or anything else that can be stored into an OCI registry)
     OciManifestDigest {
-        /// String pointing to the object (e.g.: `resitry.testing.lan/busybox:1.0.0`)
+        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
         image: String,
         /// A tokio oneshot channel over which the evaluation response has to be sent
         response_channel: oneshot::Sender<Result<CallbackResponse>>,
