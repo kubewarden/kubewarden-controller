822c379bd7062330089ee14a271c3004cfb914a7
Rafael Fernández López <rfernandezlopez@suse.com>
2022-03-21 11:57:32 +0200

    Expose `policy-fetcher` through `policy-evaluator`
    This makes easier for consumers of both crates (e.g. `kwctl` and
    `policy-server`) to consume types and traits that are common to
    both (given that `policy-evaluator` also depends on `policy-fetcher`).

--- crates/policy-evaluator/src/lib.rs
+++ crates/policy-evaluator/src/lib.rs
@@ -14,4 +14,15 @@ mod policy_tracing;
 pub mod runtimes;
 pub mod validation_response;
 
+// API's that expose other crate types (such as Kubewarden Policy SDK
+// or `policy_fetcher`) can either implement their own exposed types,
+// and means to convert those types internally to their dependencies
+// types, or depending on the specific case, re-export dependencies
+// API's directly.
+//
+// Re-exporting specific crates that belong to us is easier for common
+// consumers of these libraries along with the `policy-evaluator`, so
+// they can access these crates through the `policy-evaluator` itself,
+// streamlining their dependencies as well.
 pub use kubewarden_policy_sdk::metadata::ProtocolVersion;
+pub use policy_fetcher;
