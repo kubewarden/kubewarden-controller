a26d261b09f056fedd321b134bfbe9f9199597ca
raulcabello <raul.cabello@suse.com>
2022-03-31 16:00:18 +0200

    feat: show Sigstore signatures on kwctl inspect
    Remove oci-distribution dependency and use it from policy-fetcher

--- crates/kwctl/src/inspect.rs
+++ crates/kwctl/src/inspect.rs
@@ -1,7 +1,7 @@
 use crate::{DockerConfig, Registry, Sources};
 use anyhow::{anyhow, Result};
 use mdcat::{ResourceAccess, TerminalCapabilities, TerminalSize};
-use oci_distribution::manifest::{OciImageManifest, OciManifest};
+use policy_evaluator::policy_fetcher::oci_distribution::manifest::{OciImageManifest, OciManifest};
 use policy_evaluator::policy_fetcher::sigstore::cosign::{ClientBuilder, CosignCapabilities};
 use policy_evaluator::policy_fetcher::sigstore::registry::{Auth, ClientConfig};
 use policy_evaluator::{
