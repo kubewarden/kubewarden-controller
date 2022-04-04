ebd344a8ee86c10716e2b9ac0814bf1162223d10
José Guilherme Vanz <jguilhermevanz@suse.com>
2022-04-04 16:23:37 -0300

    Replicaset permissions.
    Adds the permissions to allow the controller to access the replicaset
    resources.

--- controllers/admissionpolicy_controller.go
+++ controllers/admissionpolicy_controller.go
@@ -40,6 +40,7 @@ import (
 //+kubebuilder:rbac:groups=policies.kubewarden.io,resources=admissionpolicies/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=policies.kubewarden.io,resources=admissionpolicies/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=apps,resources=replicasets;deployments,verbs=get;list;watch
 
 // AdmissionPolicyReconciler reconciles an AdmissionPolicy object
 type AdmissionPolicyReconciler struct {
