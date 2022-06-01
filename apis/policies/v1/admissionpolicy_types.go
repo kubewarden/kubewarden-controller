03ecdf7de5cca8ab6607e2623c45fb74b0dfb005
raulcabello <raul.cabello@suse.com>
2022-06-01 16:24:57 +0200

    feat: copy AdmissionPolicy files from v1alpha2 to v1

--- apis/policies/v1/admissionpolicy_types.go
+++ apis/policies/v1/admissionpolicy_types.go
@@ -17,37 +17,32 @@ limitations under the License.
 package v1
 
 import (
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // AdmissionPolicySpec defines the desired state of AdmissionPolicy
 type AdmissionPolicySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of AdmissionPolicy. Edit admissionpolicy_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	PolicySpec `json:""` //nolint
 }
-
-// AdmissionPolicyStatus defines the observed state of AdmissionPolicy
-type AdmissionPolicyStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
 
 // AdmissionPolicy is the Schema for the admissionpolicies API
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Namespaced
+//+kubebuilder:storageversion
+//+kubebuilder:printcolumn:name="Policy Server",type=string,JSONPath=`.spec.policyServer`,description="Bound to Policy Server"
+//+kubebuilder:printcolumn:name="Mutating",type=boolean,JSONPath=`.spec.mutating`,description="Whether the policy is mutating"
+//+kubebuilder:printcolumn:name="Mode",type=string,JSONPath=`.spec.mode`,description="Policy deployment mode"
+//+kubebuilder:printcolumn:name="Observed mode",type=string,JSONPath=`.status.mode`,description="Policy deployment mode observed on the assigned Policy Server"
+//+kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.policyStatus`,description="Status of the policy"
 type AdmissionPolicy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   AdmissionPolicySpec   `json:"spec,omitempty"`
-	Status AdmissionPolicyStatus `json:"status,omitempty"`
+	Spec   AdmissionPolicySpec `json:"spec,omitempty"`
+	Status PolicyStatus        `json:"status,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -61,4 +56,87 @@ type AdmissionPolicyList struct {
 
 func init() {
 	SchemeBuilder.Register(&AdmissionPolicy{}, &AdmissionPolicyList{})
+}
+
+func (r *AdmissionPolicy) SetStatus(status PolicyStatusEnum) {
+	r.Status.PolicyStatus = status
+}
+
+func (r *AdmissionPolicy) GetPolicyMode() PolicyMode {
+	return r.Spec.Mode
+}
+
+func (r *AdmissionPolicy) SetPolicyModeStatus(policyMode PolicyModeStatus) {
+	r.Status.PolicyMode = policyMode
+}
+
+func (r *AdmissionPolicy) GetModule() string {
+	return r.Spec.Module
+}
+
+func (r *AdmissionPolicy) IsMutating() bool {
+	return r.Spec.Mutating
+}
+
+func (r *AdmissionPolicy) GetSettings() runtime.RawExtension {
+	return r.Spec.Settings
+}
+
+func (r *AdmissionPolicy) GetStatus() *PolicyStatus {
+	return &r.Status
+}
+
+func (r *AdmissionPolicy) CopyInto(policy *Policy) {
+	*policy = r.DeepCopy()
+}
+
+func (r *AdmissionPolicy) GetSideEffects() *admissionregistrationv1.SideEffectClass {
+	return r.Spec.SideEffects
+}
+
+// GetRules returns all rules. Scope is namespaced since AdmissionPolicy just watch for namespace resources
+func (r *AdmissionPolicy) GetRules() []admissionregistrationv1.RuleWithOperations {
+	namespacedScopeV1 := admissionregistrationv1.NamespacedScope
+	rules := make([]admissionregistrationv1.RuleWithOperations, 0)
+	for _, rule := range r.Spec.Rules {
+		rule.Scope = &namespacedScopeV1
+		rules = append(rules, rule)
+	}
+
+	return rules
+}
+
+func (r *AdmissionPolicy) GetFailurePolicy() *admissionregistrationv1.FailurePolicyType {
+	return r.Spec.FailurePolicy
+}
+
+func (r *AdmissionPolicy) GetMatchPolicy() *admissionregistrationv1.MatchPolicyType {
+	return r.Spec.MatchPolicy
+}
+
+// GetNamespaceSelector returns the namespace of the AdmissionPolicy since it is the only namespace we want the policy to be applied to.
+func (r *AdmissionPolicy) GetNamespaceSelector() *metav1.LabelSelector {
+	return &metav1.LabelSelector{
+		MatchLabels: map[string]string{"kubernetes.io/metadata.name": r.ObjectMeta.Namespace},
+	}
+}
+
+func (r *AdmissionPolicy) GetObjectSelector() *metav1.LabelSelector {
+	return r.Spec.ObjectSelector
+}
+
+func (r *AdmissionPolicy) GetTimeoutSeconds() *int32 {
+	return r.Spec.TimeoutSeconds
+}
+
+func (r *AdmissionPolicy) GetObjectMeta() *metav1.ObjectMeta {
+	return &r.ObjectMeta
+}
+
+func (r *AdmissionPolicy) GetPolicyServer() string {
+	return r.Spec.PolicyServer
+}
+
+func (r *AdmissionPolicy) GetUniqueName() string {
+	return "namespaced-" + r.Namespace + "-" + r.Name
 }
---
+++ apis/policies/v1/admissionpolicy_webhook.go
@@ -0,0 +1,113 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"fmt"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.com/kubewarden/kubewarden-controller/internal/pkg/constants"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// log is for logging in this package.
+var admissionpolicylog = logf.Log.WithName("admissionpolicy-resource")
+
+func (r *AdmissionPolicy) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	err := ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+	if err != nil {
+		return fmt.Errorf("failed enrolling webhook with manager: %w", err)
+	}
+	return nil
+}
+
+//+kubebuilder:webhook:path=/mutate-policies-kubewarden-io-v1alpha2-admissionpolicy,mutating=true,failurePolicy=fail,sideEffects=None,groups=policies.kubewarden.io,resources=admissionpolicies,verbs=create;update,versions=v1alpha2,name=madmissionpolicy.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Defaulter = &AdmissionPolicy{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *AdmissionPolicy) Default() {
+	admissionpolicylog.Info("default", "name", r.Name)
+	if r.Spec.PolicyServer == "" {
+		r.Spec.PolicyServer = constants.DefaultPolicyServer
+	}
+	if r.ObjectMeta.DeletionTimestamp == nil {
+		controllerutil.AddFinalizer(r, constants.KubewardenFinalizer)
+	}
+}
+
+//+kubebuilder:webhook:path=/validate-policies-kubewarden-io-v1alpha2-admissionpolicy,mutating=false,failurePolicy=fail,sideEffects=None,groups=policies.kubewarden.io,resources=admissionpolicies,verbs=create;update,versions=v1alpha2,name=vadmissionpolicy.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Validator = &AdmissionPolicy{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *AdmissionPolicy) ValidateCreate() error {
+	admissionpolicylog.Info("validate create", "name", r.Name)
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *AdmissionPolicy) ValidateUpdate(old runtime.Object) error {
+	admissionpolicylog.Info("validate update", "name", r.Name)
+
+	oldPolicy, ok := old.(*AdmissionPolicy)
+	if !ok {
+		return apierrors.NewInternalError(
+			fmt.Errorf("object is not of type AdmissionPolicy: %#v", old))
+	}
+	return validatePolicyUpdate(oldPolicy, r)
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *AdmissionPolicy) ValidateDelete() error {
+	admissionpolicylog.Info("validate delete", "name", r.Name)
+	return nil
+}
+
+func validatePolicyUpdate(oldPolicy, newPolicy Policy) error {
+	if newPolicy.GetPolicyServer() != oldPolicy.GetPolicyServer() {
+		var errs field.ErrorList
+		p := field.NewPath("spec")
+		pp := p.Child("policyServer")
+		errs = append(errs, field.Forbidden(pp, "the field is immutable"))
+
+		return apierrors.NewInvalid(
+			schema.GroupKind{Group: GroupVersion.Group, Kind: "ClusterAdmissionPolicy"},
+			newPolicy.GetName(), errs)
+	}
+	if newPolicy.GetPolicyMode() == "monitor" && oldPolicy.GetPolicyMode() == "protect" {
+		var errs field.ErrorList
+		p := field.NewPath("spec")
+		pp := p.Child("mode")
+		errs = append(errs, field.Forbidden(pp, "field cannot transition from protect to monitor. Recreate instead."))
+
+		return apierrors.NewInvalid(
+			schema.GroupKind{Group: GroupVersion.Group, Kind: "ClusterAdmissionPolicy"},
+			newPolicy.GetName(), errs)
+	}
+
+	return nil
+}
