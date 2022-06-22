d3e4542f0b18a04acb6ca0b028672f3b34658432
Flavio Castelli <fcastelli@suse.com>
2022-06-22 11:45:07 +0200

    Remove hyper dependency
    The hyper dependency was used only to access a constant definig the HTTP
    status of for internal server error.
    
    That was kinda an overkill...
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- crates/policy-evaluator/src/admission_response.rs
+++ crates/policy-evaluator/src/admission_response.rs
@@ -63,11 +63,7 @@ impl AdmissionResponse {
     }
 
     pub fn reject_internal_server_error(uid: String, message: String) -> AdmissionResponse {
-        AdmissionResponse::reject(
-            uid,
-            format!("internal server error: {}", message),
-            hyper::StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
-        )
+        AdmissionResponse::reject(uid, format!("internal server error: {}", message), 500)
     }
 
     pub fn from_policy_validation_response(
