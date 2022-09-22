bd17054cb3db80266a30a782038d3749bd6e96d1
Flavio Castelli <fcastelli@suse.com>
2022-09-22 09:37:02 +0200

    refactoring: small changes to PolicyEvaluatorBuilder
    Move code from the non-public `PolicyEvaluator::new` methods into the
    `PolicyEvaluatorBuilder` structure.
    
    Also, allow users to provide an already instantiate `wasmtime::Module`
    to `PolicyEvaluatorBuilder`, this is required to reduce the bootstrap
    time of Policy Server.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- crates/policy-evaluator/src/policy_evaluator_builder.rs
+++ crates/policy-evaluator/src/policy_evaluator_builder.rs
@@ -1,19 +1,26 @@
 use anyhow::{anyhow, Result};
-use std::{fs, path::Path};
-use tokio::sync::mpsc::Sender;
+use std::convert::TryInto;
+use std::path::Path;
+use tokio::sync::mpsc;
+use wapc::WapcHost;
+use wasmtime_provider::{wasmtime, WasmtimeEngineProviderBuilder};
 
 use crate::callback_requests::CallbackRequest;
-use crate::policy_evaluator::{PolicyEvaluator, PolicyExecutionMode};
+use crate::policy::Policy;
+use crate::policy_evaluator::{BurregoEvaluator, PolicyEvaluator, PolicyExecutionMode, Runtime};
+use crate::runtimes::{wapc::host_callback as wapc_callback, wapc::WAPC_POLICY_MAPPING};
 
 /// Helper Struct that creates a `PolicyEvaluator` object
 #[derive(Default)]
 pub struct PolicyEvaluatorBuilder {
+    engine: Option<wasmtime::Engine>,
     policy_id: String,
     policy_file: Option<String>,
     policy_contents: Option<Vec<u8>>,
+    policy_module: Option<wasmtime::Module>,
     execution_mode: Option<PolicyExecutionMode>,
     settings: Option<serde_json::Map<String, serde_json::Value>>,
-    callback_channel: Option<Sender<CallbackRequest>>,
+    callback_channel: Option<mpsc::Sender<CallbackRequest>>,
     wasmtime_cache: bool,
 }
 
@@ -25,6 +32,17 @@ impl PolicyEvaluatorBuilder {
             policy_id,
             ..Default::default()
         }
+    }
+
+    /// [`wasmtime::Engine`] instance to be used when creating the
+    /// policy evaluator
+    ///
+    /// **Warning:** when used, all the [`wasmtime::Engine`] specific settings
+    /// must be set by the caller when creating the engine.
+    /// This includes options like: cache, epoch counter
+    pub fn engine(mut self, engine: wasmtime::Engine) -> Self {
+        self.engine = Some(engine);
+        self
     }
 
     /// Build the policy by reading the Wasm file from disk.
@@ -42,6 +60,14 @@ impl PolicyEvaluatorBuilder {
     /// Cannot be used at the same time as `policy_file`
     pub fn policy_contents(mut self, data: &[u8]) -> PolicyEvaluatorBuilder {
         self.policy_contents = Some(data.to_owned());
+        self
+    }
+
+    /// Use a pre-built [`wasmtime::Module`] instance.
+    /// **Warning:** you must provide also the [`wasmtime::Engine`] used
+    /// to allocate the `Module`, otherwise the code will panic at runtime
+    pub fn policy_module(mut self, module: wasmtime::Module) -> Self {
+        self.policy_module = Some(module);
         self
     }
 
@@ -74,41 +100,223 @@ impl PolicyEvaluatorBuilder {
     /// some waPC functions exposed by the host will not be available at runtime.
     /// The policy evaluation will not fail because of that, but the guest will
     /// get an error instead of the expected result.
-    pub fn callback_channel(mut self, channel: Sender<CallbackRequest>) -> PolicyEvaluatorBuilder {
+    pub fn callback_channel(
+        mut self,
+        channel: mpsc::Sender<CallbackRequest>,
+    ) -> PolicyEvaluatorBuilder {
         self.callback_channel = Some(channel);
         self
     }
 
-    /// Create the instance of `PolicyEvaluator` to be used
-    pub fn build(self) -> Result<PolicyEvaluator> {
+    /// Ensure the configuration provided to the build is correct
+    fn validate_user_input(&self) -> Result<()> {
         if self.policy_file.is_some() && self.policy_contents.is_some() {
             return Err(anyhow!(
                 "Cannot specify 'policy_file' and 'policy_contents' at the same time"
             ));
         }
-        if self.policy_file.is_none() && self.policy_contents.is_none() {
+        if self.policy_file.is_some() && self.policy_module.is_some() {
             return Err(anyhow!(
-                "Must specify either 'policy_file' or 'policy_contents'"
+                "Cannot specify 'policy_file' and 'policy_module' at the same time"
             ));
         }
-        let contents: Vec<u8> = if let Some(file) = self.policy_file {
-            fs::read(file.clone())
-                .map_err(|e| anyhow!("Cannot read policy from file {}: {:?}", file, e))?
+        if self.policy_contents.is_some() && self.policy_module.is_some() {
+            return Err(anyhow!(
+                "Cannot specify 'policy_contents' and 'policy_module' at the same time"
+            ));
+        }
+
+        if self.policy_file.is_none()
+            && self.policy_contents.is_none()
+            && self.policy_module.is_none()
+        {
+            return Err(anyhow!(
+                "Must specify one among: `policy_file`, `policy_contents` and `policy_module`"
+            ));
+        }
+
+        if self.engine.is_none() && self.policy_module.is_some() {
+            return Err(anyhow!(
+                "You must provide the `engine` that was used to instantiate the given `policy_module`"
+            ));
+        }
+
+        if self.execution_mode.is_none() {
+            return Err(anyhow!("Must specify execution mode"));
+        }
+
+        Ok(())
+    }
+
+    /// Create the instance of `PolicyEvaluator` to be used
+    pub fn build(&self) -> Result<PolicyEvaluator> {
+        self.validate_user_input()?;
+
+        let engine = self
+            .engine
+            .as_ref()
+            .map_or_else(
+                || {
+                    let mut wasmtime_config = wasmtime::Config::new();
+                    if self.wasmtime_cache {
+                        wasmtime_config.cache_config_load_default()?;
+                    }
+
+                    wasmtime::Engine::new(&wasmtime_config)
+                },
+                |e| Ok(e.clone()),
+            )
+            .map_err(|e| anyhow!("cannot create wasmtime engine: {:?}", e))?;
+
+        let module: wasmtime::Module = if let Some(m) = &self.policy_module {
+            // it's fine to clone a Module, this is a cheap operation that just
+            // copies its internal reference. See wasmtime docs
+            m.clone()
         } else {
-            self.policy_contents.unwrap()
+            match &self.policy_file {
+                Some(file) => wasmtime::Module::from_file(&engine, file),
+                None => wasmtime::Module::new(&engine, self.policy_contents.as_ref().unwrap()),
+            }?
         };
 
-        let mode = self
-            .execution_mode
-            .ok_or_else(|| anyhow!("Must specify execution mode"))?;
+        let execution_mode = self.execution_mode.unwrap();
 
-        PolicyEvaluator::new(
-            self.policy_id,
-            contents,
-            mode,
-            self.settings,
-            self.callback_channel,
-            self.wasmtime_cache,
-        )
+        let (policy, runtime) = match execution_mode {
+            PolicyExecutionMode::KubewardenWapc => {
+                let engine_provider = WasmtimeEngineProviderBuilder::new()
+                    .engine(engine)
+                    .module(module)
+                    .build()?;
+
+                let wapc_host =
+                    WapcHost::new(Box::new(engine_provider), Some(Box::new(wapc_callback)))?;
+                let policy = Self::from_contents_internal(
+                    self.policy_id.clone(),
+                    self.callback_channel.clone(),
+                    || Some(wapc_host.id()),
+                    Policy::new,
+                    execution_mode,
+                )?;
+
+                let policy_runtime = Runtime::Wapc(wapc_host);
+                (policy, policy_runtime)
+            }
+            PolicyExecutionMode::Opa | PolicyExecutionMode::OpaGatekeeper => {
+                let policy = Self::from_contents_internal(
+                    self.policy_id.clone(),
+                    self.callback_channel.clone(),
+                    || None,
+                    Policy::new,
+                    execution_mode,
+                )?;
+
+                let evaluator = burrego::Evaluator::from_engine_and_module(
+                    engine,
+                    module,
+                    crate::runtimes::burrego::new_host_callbacks(),
+                )?;
+
+                let policy_runtime = Runtime::Burrego(Box::new(BurregoEvaluator {
+                    evaluator,
+                    entrypoint_id: 0, // currently hardcoded to this value
+                    policy_execution_mode: execution_mode.try_into()?,
+                }));
+
+                (policy, policy_runtime)
+            }
+        };
+
+        Ok(PolicyEvaluator {
+            runtime,
+            policy,
+            settings: self.settings.clone().unwrap_or_default(),
+        })
+    }
+
+    fn from_contents_internal<E, P>(
+        id: String,
+        callback_channel: Option<mpsc::Sender<CallbackRequest>>,
+        engine_initializer: E,
+        policy_initializer: P,
+        policy_execution_mode: PolicyExecutionMode,
+    ) -> Result<Policy>
+    where
+        E: Fn() -> Option<u64>,
+        P: Fn(String, Option<u64>, Option<mpsc::Sender<CallbackRequest>>) -> Result<Policy>,
+    {
+        let instance_id = engine_initializer();
+        let policy = policy_initializer(id, instance_id, callback_channel)?;
+        if policy_execution_mode == PolicyExecutionMode::KubewardenWapc {
+            WAPC_POLICY_MAPPING
+                .write()
+                .expect("cannot write to global WAPC_POLICY_MAPPING")
+                .insert(
+                    instance_id.ok_or_else(|| anyhow!("invalid policy id"))?,
+                    policy.clone(),
+                );
+        }
+        Ok(policy)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn policy_is_registered_in_the_mapping() -> Result<()> {
+        let policy_name = "policy_is_registered_in_the_mapping";
+
+        // We cannot set policy.id at build time, because some attributes
+        // of Policy are private.
+        let mut policy = Policy::default();
+        policy.id = policy_name.to_string();
+
+        let policy_id = 1;
+
+        PolicyEvaluatorBuilder::from_contents_internal(
+            "mock_policy".to_string(),
+            None,
+            || Some(policy_id),
+            |_, _, _| Ok(policy.clone()),
+            PolicyExecutionMode::KubewardenWapc,
+        )?;
+
+        let policy_mapping = WAPC_POLICY_MAPPING.read().unwrap();
+        let found = policy_mapping
+            .iter()
+            .find(|(_id, policy)| policy.id == policy_name);
+
+        assert!(found.is_some());
+
+        Ok(())
+    }
+
+    #[test]
+    fn policy_is_not_registered_in_the_mapping_if_not_wapc() -> Result<()> {
+        let policy_name = "policy_is_not_registered_in_the_mapping_if_not_wapc";
+
+        // We cannot set policy.id at build time, because some attributes
+        // of Policy are private.
+        let mut policy = Policy::default();
+        policy.id = policy_name.to_string();
+
+        let policy_id = 1;
+
+        PolicyEvaluatorBuilder::from_contents_internal(
+            policy_name.to_string(),
+            None,
+            || Some(policy_id),
+            |_, _, _| Ok(policy.clone()),
+            PolicyExecutionMode::OpaGatekeeper,
+        )?;
+
+        let policy_mapping = WAPC_POLICY_MAPPING.read().unwrap();
+        let found = policy_mapping
+            .iter()
+            .find(|(_id, policy)| policy.id == policy_name);
+
+        assert!(found.is_none());
+        Ok(())
     }
 }
