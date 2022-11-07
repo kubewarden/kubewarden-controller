383b04fb13313f83ec0c17dd3f7824201a520c6b
raulcabello <raul.cabello@suse.com>
2022-11-07 10:04:01 +0100

    Add unit test for GetNamespaceSelector method
    Signed-off-by: raulcabello <raul.cabello@suse.com>

---
+++ apis/policies/v1/clusteradmissionpolicy_types_test.go
@@ -0,0 +1,55 @@
+package v1
+
+import (
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func TestGetNamespaceSelectorWithEmptyNamespaceSelector(t *testing.T) {
+	kubewardenNs := "kubewarden"
+	c := ClusterAdmissionPolicy{}
+	nsSelector := c.GetNamespaceSelector(kubewardenNs)
+	isKubewardenNsFound := isNamespaceFoundInSelector(nsSelector, kubewardenNs)
+
+	if !isKubewardenNsFound {
+		t.Errorf("Kubewarden namespace not added to namespace selector")
+	}
+}
+
+func TestGetNamespaceSelectorWithExistingMatchExpressions(t *testing.T) {
+	kubewardenNs := "kubewarden"
+	c := ClusterAdmissionPolicy{
+		Spec: ClusterAdmissionPolicySpec{
+			NamespaceSelector: &v1.LabelSelector{
+				MatchExpressions: []v1.LabelSelectorRequirement{
+					{
+						Key:      "In",
+						Operator: "kubernetes.io/metadata.name",
+						Values:   []string{"foo"},
+					},
+				},
+			},
+		},
+	}
+	nsSelector := c.GetNamespaceSelector(kubewardenNs)
+	isKubewardenNsFound := isNamespaceFoundInSelector(nsSelector, kubewardenNs)
+
+	if !isKubewardenNsFound {
+		t.Errorf("Kubewarden namespace not added to namespace selector")
+	}
+}
+
+func isNamespaceFoundInSelector(selector *v1.LabelSelector, namespace string) bool {
+	isKubewardenNsFound := false
+
+	for _, matchExpression := range selector.MatchExpressions {
+		if len(matchExpression.Values) == 1 &&
+			matchExpression.Values[0] == namespace &&
+			matchExpression.Key == "kubernetes.io/metadata.name" &&
+			matchExpression.Operator == "NotIn" {
+			isKubewardenNsFound = true
+		}
+	}
+
+	return isKubewardenNsFound
+}
