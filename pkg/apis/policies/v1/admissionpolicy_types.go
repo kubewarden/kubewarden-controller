423cefd71723bf3606737586d0cfd2a79cb0c301
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2023-06-13 09:20:01 +0200

    feat: Add Age, Severity, Category to printer columns
    Severity and Category are priority 1, hence they will get cut if
    terminal is too narrow.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/apis/policies/v1/admissionpolicy_types.go
+++ pkg/apis/policies/v1/admissionpolicy_types.go
@@ -38,6 +38,9 @@ type AdmissionPolicySpec struct {
 // +kubebuilder:printcolumn:name="Mode",type=string,JSONPath=`.spec.mode`,description="Policy deployment mode"
 // +kubebuilder:printcolumn:name="Observed mode",type=string,JSONPath=`.status.mode`,description="Policy deployment mode observed on the assigned Policy Server"
 // +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.policyStatus`,description="Status of the policy"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="Severity",type=string,JSONPath=".metadata.annotations['io\\.kubewarden\\.policy\\.severity']",priority=1
+// +kubebuilder:printcolumn:name="Category",type=string,JSONPath=".metadata.annotations['io\\.kubewarden\\.policy\\.category']",priority=1
 type AdmissionPolicy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
