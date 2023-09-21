fff44338eb637a2e101b3767d096d70effa9bcfb
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-09-21 18:42:59 +0200

    fix: do not skip cache
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/pkg/policyserver/validation.go
+++ internal/pkg/policyserver/validation.go
@@ -5,34 +5,18 @@ import (
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // ValidateImagePullSecret validates that the specified PolicyServer imagePullSecret exists and is of type kubernetes.io/dockerconfigjson
 func ValidateImagePullSecret(ctx context.Context, k8sClient client.Client, imagePullSecret string, deploymentsNamespace string) error {
-	// By using Unstructured data we force the client to fetch fresh, uncached
-	// data from the API server
-	unstructuredObj := &unstructured.Unstructured{}
-	unstructuredObj.SetGroupVersionKind(schema.GroupVersionKind{
-		Kind:    "Secret",
-		Version: "v1",
-	})
+	secret := &corev1.Secret{}
 	err := k8sClient.Get(ctx, client.ObjectKey{
 		Namespace: deploymentsNamespace,
 		Name:      imagePullSecret,
-	}, unstructuredObj)
+	}, secret)
 	if err != nil {
 		return fmt.Errorf("cannot get spec.ImagePullSecret: %w", err)
-	}
-
-	var secret corev1.Secret
-	err = runtime.DefaultUnstructuredConverter.
-		FromUnstructured(unstructuredObj.UnstructuredContent(), &secret)
-	if err != nil {
-		return fmt.Errorf("spec.ImagePullSecret is not of Kind Secret: %w", err)
 	}
 
 	if secret.Type != "kubernetes.io/dockerconfigjson" {
