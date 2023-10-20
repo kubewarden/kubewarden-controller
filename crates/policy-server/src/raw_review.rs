4f30448410c6ceb7b61c9f16699e5973e89b4be3
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-10-20 16:08:38 +0200

    docs: add docstrings
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- crates/policy-server/src/raw_review.rs
+++ crates/policy-server/src/raw_review.rs
@@ -1,11 +1,13 @@
 use policy_evaluator::admission_response::AdmissionResponse;
 use serde::{Deserialize, Serialize};
 
+/// A review request that contains a raw json value.
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct RawReviewRequest {
     pub(crate) request: serde_json::Value,
 }
 
+/// A review response from a raw policy evaluation.
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct RawReviewResponse {
     pub(crate) response: AdmissionResponse,
