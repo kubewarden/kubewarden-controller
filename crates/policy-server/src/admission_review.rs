07cd35bc763b7a45144934732f77f2c7c67b881c
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-10-20 12:44:24 +0200

    refactor: move AdmissionRequest to policy-evaluator
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- crates/policy-server/src/admission_review.rs
+++ crates/policy-server/src/admission_review.rs
@@ -1,18 +1,5 @@
+use policy_evaluator::admission_request::AdmissionRequest;
 use policy_evaluator::admission_response::AdmissionResponse;
-
-#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
-pub(crate) struct GroupVersionKind {
-    pub group: String,
-    pub version: String,
-    pub kind: String,
-}
-
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-pub(crate) struct GroupVersionResource {
-    pub group: String,
-    pub version: String,
-    pub resource: String,
-}
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -49,37 +36,6 @@ impl Default for AdmissionReview {
         }
     }
 }
-
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub(crate) struct AdmissionRequest {
-    pub uid: String,
-    pub kind: GroupVersionKind,
-    pub resource: GroupVersionResource,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub sub_resource: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub request_kind: Option<GroupVersionKind>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub request_resource: Option<GroupVersionResource>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub request_sub_resource: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub namespace: Option<String>,
-    pub operation: String,
-    pub user_info: k8s_openapi::api::authentication::v1::UserInfo,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub object: Option<k8s_openapi::apimachinery::pkg::runtime::RawExtension>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub old_object: Option<k8s_openapi::apimachinery::pkg::runtime::RawExtension>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub dry_run: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub options: Option<k8s_openapi::apimachinery::pkg::runtime::RawExtension>,
-}
-
 #[cfg(test)]
 pub(crate) mod tests {
     use super::*;
