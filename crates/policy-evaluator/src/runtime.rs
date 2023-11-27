b3fb2c3be9cd60b7ab8d571751e1fa782e593f83
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-11-27 21:55:18 +0100

    chore: remove unused module
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- crates/policy-evaluator/src/runtime.rs
+++
@@ -1,6 +0,0 @@
-enum Runtime {
-    Wapc(wapc::WapcHost),
-    // The `BurregoEvaluator` variant is boxed since it outsizes the
-    // other variants of this enum.
-    Burrego(Box<BurregoEvaluator>),
-}
