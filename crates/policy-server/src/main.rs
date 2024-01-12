0a44c7f503bb83ef07732f9276c8e0b20525ca7c
Fabrizio Sestito <fabrizio.sestito@suse.com>
2024-01-12 13:35:15 +0100

    refactor: update main
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- crates/policy-server/src/main.rs
+++ crates/policy-server/src/main.rs
@@ -1,10 +1,49 @@
 mod cli;
+mod tracing;
 
+use std::fs;
+
+use ::tracing::info;
+use anyhow::anyhow;
 use anyhow::Result;
+use opentelemetry::global::shutdown_tracer_provider;
+use policy_server::PolicyServer;
 
-fn main() -> Result<()> {
+use crate::tracing::setup_tracing;
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let matches = cli::build_cli().get_matches();
     let config = policy_server::config::Config::from_args(&matches)?;
 
-    policy_server::run(config)
+    setup_tracing(&config.log_level, &config.log_fmt, config.log_no_color)?;
+
+    if config.daemon {
+        info!("Running instance as a daemon");
+
+        let mut daemonize = daemonize::Daemonize::new().pid_file(&config.daemon_pid_file);
+        if let Some(stdout_file) = config.daemon_stdout_file.clone() {
+            let file = fs::File::create(stdout_file)
+                .map_err(|e| anyhow!("Cannot create file for daemon stdout: {}", e))?;
+            daemonize = daemonize.stdout(file);
+        }
+        if let Some(stderr_file) = config.daemon_stderr_file.clone() {
+            let file = fs::File::create(stderr_file)
+                .map_err(|e| anyhow!("Cannot create file for daemon stderr: {}", e))?;
+            daemonize = daemonize.stderr(file);
+        }
+
+        daemonize
+            .start()
+            .map_err(|e| anyhow!("Cannot daemonize: {}", e))?;
+
+        info!("Detached from shell, now running in background.");
+    }
+
+    let api_server = PolicyServer::new_from_config(config).await?;
+    api_server.run().await?;
+
+    shutdown_tracer_provider();
+
+    Ok(())
 }
