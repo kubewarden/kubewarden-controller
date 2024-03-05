27dd7ae1acbd211345f3f256b90e65e67ad4f09c
Flavio Castelli <fcastelli@suse.com>
2024-03-05 18:43:02 +0100

    fix: show cli options sorted
    Sort the cli options when the `--help` command is used
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- crates/policy-server/src/cli.rs
+++ crates/policy-server/src/cli.rs
@@ -21,11 +21,7 @@ lazy_static! {
 }
 
 pub(crate) fn build_cli() -> Command {
-    Command::new(crate_name!())
-        .author(crate_authors!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .arg(
+    let mut args = vec![
             Arg::new("log-level")
                 .long("log-level")
                 .value_name("LOG_LEVEL")
@@ -39,8 +35,6 @@ pub(crate) fn build_cli() -> Command {
                     PossibleValue::new("error"),
                 ])
                 .help("Log level"),
-        )
-        .arg(
             Arg::new("log-fmt")
                 .long("log-fmt")
                 .value_name("LOG_FMT")
@@ -52,169 +46,132 @@ pub(crate) fn build_cli() -> Command {
                     PossibleValue::new("otlp"),
                 ])
                 .help("Log output format"),
-        )
-        .arg(
             Arg::new("log-no-color")
                 .long("log-no-color")
                 .env("NO_COLOR")
                 .required(false)
                 .help("Disable colored output for logs"),
-        )
-        .arg(
             Arg::new("address")
                 .long("addr")
                 .value_name("BIND_ADDRESS")
                 .default_value("0.0.0.0")
                 .env("KUBEWARDEN_BIND_ADDRESS")
                 .help("Bind against ADDRESS"),
-        )
-        .arg(
             Arg::new("port")
                 .long("port")
                 .value_name("PORT")
                 .default_value("3000")
                 .env("KUBEWARDEN_PORT")
                 .help("Listen on PORT"),
-        )
-        .arg(
             Arg::new("workers")
                 .long("workers")
                 .value_name("WORKERS_NUMBER")
                 .env("KUBEWARDEN_WORKERS")
                 .help("Number of workers thread to create"),
-        )
-        .arg(
             Arg::new("cert-file")
                 .long("cert-file")
                 .value_name("CERT_FILE")
                 .default_value("")
                 .env("KUBEWARDEN_CERT_FILE")
                 .help("Path to an X.509 certificate file for HTTPS"),
-        )
-        .arg(
             Arg::new("key-file")
                 .long("key-file")
                 .value_name("KEY_FILE")
                 .default_value("")
                 .env("KUBEWARDEN_KEY_FILE")
                 .help("Path to an X.509 private key file for HTTPS"),
-        )
-        .arg(
             Arg::new("policies")
                 .long("policies")
                 .value_name("POLICIES_FILE")
                 .env("KUBEWARDEN_POLICIES")
                 .default_value("policies.yml")
                 .help("YAML file holding the policies to be loaded and their settings"),
-        )
-        .arg(
             Arg::new("policies-download-dir")
                 .long("policies-download-dir")
                 .value_name("POLICIES_DOWNLOAD_DIR")
                 .default_value(".")
                 .env("KUBEWARDEN_POLICIES_DOWNLOAD_DIR")
                 .help("Download path for the policies"),
-        )
-        .arg(
             Arg::new("sigstore-cache-dir")
                 .long("sigstore-cache-dir")
                 .value_name("SIGSTORE_CACHE_DIR")
                 .default_value("sigstore-data")
                 .env("KUBEWARDEN_SIGSTORE_CACHE_DIR")
                 .help("Directory used to cache sigstore data"),
-        )
-        .arg(
             Arg::new("sources-path")
                 .long("sources-path")
                 .value_name("SOURCES_PATH")
                 .env("KUBEWARDEN_SOURCES_PATH")
                 .help("YAML file holding source information (https, registry insecure hosts, custom CA's...)"),
-        )
-        .arg(
             Arg::new("verification-path")
                 .long("verification-path")
                 .value_name("VERIFICATION_CONFIG_PATH")
                 .env("KUBEWARDEN_VERIFICATION_CONFIG_PATH")
                 .help("YAML file holding verification information (URIs, keys, annotations...)"),
-        )
-        .arg(
             Arg::new("docker-config-json-path")
                 .long("docker-config-json-path")
                 .value_name("DOCKER_CONFIG")
                 .env("KUBEWARDEN_DOCKER_CONFIG_JSON_PATH")
                 .help("Path to a Docker config.json-like path. Can be used to indicate registry authentication details"),
-        )
-        .arg(
             Arg::new("enable-metrics")
                 .long("enable-metrics")
                 .env("KUBEWARDEN_ENABLE_METRICS")
                 .required(false)
                 .help("Enable metrics"),
-        )
-        .arg(
             Arg::new("enable-verification")
                 .long("enable-verification")
                 .env("KUBEWARDEN_ENABLE_VERIFICATION")
                 .required(false)
                 .help("Enable Sigstore verification"),
-        )
-        .arg(
             Arg::new("always-accept-admission-reviews-on-namespace")
                 .long("always-accept-admission-reviews-on-namespace")
                 .value_name("NAMESPACE")
                 .env("KUBEWARDEN_ALWAYS_ACCEPT_ADMISSION_REVIEWS_ON_NAMESPACE")
                 .required(false)
                 .help("Always accept AdmissionReviews that target the given namespace"),
-        )
-        .arg(
             Arg::new("disable-timeout-protection")
                 .long("disable-timeout-protection")
                 .env("KUBEWARDEN_DISABLE_TIMEOUT_PROTECTION")
                 .required(false)
                 .help("Disable policy timeout protection"),
-        )
-        .arg(
             Arg::new("policy-timeout")
                 .long("policy-timeout")
                 .env("KUBEWARDEN_POLICY_TIMEOUT")
                 .value_name("MAXIMUM_EXECUTION_TIME_SECONDS")
                 .default_value("2")
                 .help("Interrupt policy evaluation after the given time"),
-        )
-        .arg(
             Arg::new("daemon")
                 .long("daemon")
                 .env("KUBEWARDEN_DAEMON")
                 .required(false)
                 .help("If set, runs policy-server in detached mode as a daemon"),
-        )
-        .arg(
             Arg::new("daemon-pid-file")
                 .long("daemon-pid-file")
                 .env("KUBEWARDEN_DAEMON_PID_FILE")
                 .default_value("policy-server.pid")
                 .help("Path to pid file, used only when running in daemon mode"),
-        )
-        .arg(
             Arg::new("daemon-stdout-file")
                 .long("daemon-stdout-file")
                 .env("KUBEWARDEN_DAEMON_STDOUT_FILE")
                 .required(false)
                 .help("Path to file holding stdout, used only when running in daemon mode"),
-        )
-        .arg(
             Arg::new("daemon-stderr-file")
                 .long("daemon-stderr-file")
                 .env("KUBEWARDEN_DAEMON_STDERR_FILE")
                 .required(false)
                 .help("Path to file holding stderr, used only when running in daemon mode"),
-        )
-        .arg(
             Arg::new("ignore-kubernetes-connection-failure")
                 .long("ignore-kubernetes-connection-failure")
                 .env("KUBEWARDEN_IGNORE_KUBERNETES_CONNECTION_FAILURE")
                 .required(false)
                 .help("Do not exit with an error if the Kubernetes connection fails. This will cause context aware policies to break when there's no connection with Kubernetes."),
-        )
+    ];
+    args.sort_by(|a, b| a.get_id().cmp(b.get_id()));
+
+    Command::new(crate_name!())
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about(crate_description!())
         .long_version(VERSION_AND_BUILTINS.as_str())
+        .args(args)
 }
