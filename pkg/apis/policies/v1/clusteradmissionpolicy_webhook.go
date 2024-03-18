002f25728a580732c1a405fff422d2277b9958f6
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2024-03-18 15:37:24 +0100

    chore: Remove comment, gets shipped in crd docs
    crd-ref-docs ships this comment as part of the CRD docs when
    doing `cd docs/crds; make generate`
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/apis/policies/v1/clusteradmissionpolicy_webhook.go
+++ pkg/apis/policies/v1/clusteradmissionpolicy_webhook.go
@@ -12,9 +12,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This linter directive is added to avoid issues with the
-// admissionpolicy_webhook.go file.
-//
 //nolint:dupl
 package v1
 
