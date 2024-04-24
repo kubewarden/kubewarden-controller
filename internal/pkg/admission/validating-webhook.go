4d2617a4a516bc4c4e22649fcb371563ed235e63
José Guilherme Vanz <jguilhermevanz@suse.com>
2024-04-23 23:37:17 -0300

    fix: use CreateOrUpdate to reconcile webhooks.
    Updates the controller to use the controller-runtime help function
    CreateOrUpdate to reconcile the validation and mutating webhooks.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- internal/pkg/admission/validating-webhook.go
+++ internal/pkg/admission/validating-webhook.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
-	"reflect"
 
 	policiesv1 "github.com/kubewarden/kubewarden-controller/pkg/apis/policies/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -25,96 +23,42 @@ func (r *Reconciler) ReconcileValidatingWebhookConfiguration(
 	admissionSecret *corev1.Secret,
 	policyServerNameWithPrefix string,
 ) error {
-	webhook := r.validatingWebhookConfiguration(policy, admissionSecret, policyServerNameWithPrefix)
-	err := r.Client.Create(ctx, webhook)
-	if err == nil {
-		return nil
-	}
-	if apierrors.IsAlreadyExists(err) {
-		return r.updateValidatingWebhook(ctx, policy, webhook)
-	}
-	return fmt.Errorf("cannot reconcile validating webhook: %w", err)
-}
-
-func (r *Reconciler) updateValidatingWebhook(ctx context.Context,
-	policy policiesv1.Policy,
-	newWebhook *admissionregistrationv1.ValidatingWebhookConfiguration,
-) error {
-	var originalWebhook admissionregistrationv1.ValidatingWebhookConfiguration
-
-	err := r.Client.Get(ctx, client.ObjectKey{
-		Name: policy.GetUniqueName(),
-	}, &originalWebhook)
-	if err != nil && apierrors.IsNotFound(err) {
-		return fmt.Errorf("cannot retrieve validating webhook: %w", err)
-	}
-
-	patch := originalWebhook.DeepCopy()
-
-	if patch.ObjectMeta.Labels == nil {
-		patch.ObjectMeta.Labels = make(map[string]string)
-	}
-	for key, value := range newWebhook.ObjectMeta.Labels {
-		patch.ObjectMeta.Labels[key] = value
-	}
-
-	if patch.ObjectMeta.Annotations == nil {
-		patch.ObjectMeta.Annotations = make(map[string]string)
-	}
-	for key, value := range newWebhook.ObjectMeta.Annotations {
-		patch.ObjectMeta.Annotations[key] = value
-	}
-	if !reflect.DeepEqual(originalWebhook.Webhooks, newWebhook.Webhooks) {
-		patch.Webhooks = newWebhook.Webhooks
-	}
-
-	err = r.Client.Patch(ctx, patch, client.MergeFrom(&originalWebhook))
-	if err != nil {
-		return fmt.Errorf("cannot patch validating webhook: %w", err)
-	}
-
-	return nil
-}
-
-func (r *Reconciler) validatingWebhookConfiguration(
-	policy policiesv1.Policy,
-	admissionSecret *corev1.Secret,
-	policyServerName string,
-) *admissionregistrationv1.ValidatingWebhookConfiguration {
-	admissionPath := filepath.Join("/validate", policy.GetUniqueName())
-	admissionPort := int32(constants.PolicyServerPort)
-
-	service := admissionregistrationv1.ServiceReference{
-		Namespace: r.DeploymentsNamespace,
-		Name:      policyServerName,
-		Path:      &admissionPath,
-		Port:      &admissionPort,
-	}
-
-	sideEffects := policy.GetSideEffects()
-	if sideEffects == nil {
-		noneSideEffects := admissionregistrationv1.SideEffectClassNone
-		sideEffects = &noneSideEffects
-	}
-
-	policyScope := "namespace"
-	if policy.GetNamespace() == "" {
-		policyScope = "cluster"
-	}
-
-	return &admissionregistrationv1.ValidatingWebhookConfiguration{
+	webhook := &admissionregistrationv1.ValidatingWebhookConfiguration{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: policy.GetUniqueName(),
-			Labels: map[string]string{
-				"kubewarden": "true",
-				constants.WebhookConfigurationPolicyScopeLabelKey: policyScope,
-			},
-			Annotations: map[string]string{
-				constants.WebhookConfigurationPolicyNameAnnotationKey:      policy.GetName(),
-				constants.WebhookConfigurationPolicyNamespaceAnnotationKey: policy.GetNamespace(),
-			},
 		},
-		Webhooks: []admissionregistrationv1.ValidatingWebhook{
+	}
+	_, err := controllerutil.CreateOrUpdate(ctx, r.Client, webhook, func() error {
+		admissionPath := filepath.Join("/validate", policy.GetUniqueName())
+		admissionPort := int32(constants.PolicyServerPort)
+
+		service := admissionregistrationv1.ServiceReference{
+			Namespace: r.DeploymentsNamespace,
+			Name:      policyServerNameWithPrefix,
+			Path:      &admissionPath,
+			Port:      &admissionPort,
+		}
+
+		sideEffects := policy.GetSideEffects()
+		if sideEffects == nil {
+			noneSideEffects := admissionregistrationv1.SideEffectClassNone
+			sideEffects = &noneSideEffects
+		}
+
+		policyScope := "namespace"
+		if policy.GetNamespace() == "" {
+			policyScope = "cluster"
+		}
+		webhook.Name = policy.GetUniqueName()
+		webhook.Labels = map[string]string{
+			"kubewarden": "true",
+			constants.WebhookConfigurationPolicyScopeLabelKey: policyScope,
+		}
+		webhook.Annotations = map[string]string{
+			constants.WebhookConfigurationPolicyNameAnnotationKey:      policy.GetName(),
+			constants.WebhookConfigurationPolicyNamespaceAnnotationKey: policy.GetNamespace(),
+		}
+		webhook.Webhooks = []admissionregistrationv1.ValidatingWebhook{
 			{
 				Name: fmt.Sprintf("%s.kubewarden.admission", policy.GetUniqueName()),
 				ClientConfig: admissionregistrationv1.WebhookClientConfig{
@@ -130,6 +74,11 @@ func (r *Reconciler) validatingWebhookConfiguration(
 				TimeoutSeconds:          policy.GetTimeoutSeconds(),
 				AdmissionReviewVersions: []string{"v1"},
 			},
-		},
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("cannot reconcile validating webhook: %w", err)
 	}
+	return nil
 }
