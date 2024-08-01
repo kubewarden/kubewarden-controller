ea5d6e569f23ada7a663ac53c2dc8c1866f75078
Fabrizio Sestito <fabrizio.sestito@suse.com>
2024-08-01 08:42:19 +0200

    feat: update and clean-up cert utilities
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/certs/certs.go
+++ internal/certs/certs.go
@@ -4,15 +4,13 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
 	"math/big"
-	"net"
 	"time"
-
-	"github.com/kubewarden/kubewarden-controller/internal/constants"
 )
 
 const (
@@ -21,9 +19,9 @@ const (
 	exp     = 159
 )
 
-// GenerateCA generates a self-signed CA root certificate and private key
-// The certificate is valid for 10 years.
-func GenerateCA() ([]byte, *rsa.PrivateKey, error) {
+// GenerateCA generates a self-signed CA root certificate and private key in PEM format.
+// It accepts validity bounds as parameters.
+func GenerateCA(notBefore, notAfter time.Time) ([]byte, []byte, error) {
 	serialNumber, err := rand.Int(rand.Reader, (&big.Int{}).Exp(big.NewInt(base), big.NewInt(exp), nil))
 	if err != nil {
 		return nil, nil, fmt.Errorf("cannot init serial number: %w", err)
@@ -44,8 +42,8 @@ func GenerateCA() ([]byte, *rsa.PrivateKey, error) {
 			StreetAddress: []string{""},
 			PostalCode:    []string{""},
 		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(constants.CertExpirationYears, 0, 0),
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
 		IsCA:                  true,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
@@ -62,19 +60,37 @@ func GenerateCA() ([]byte, *rsa.PrivateKey, error) {
 		return nil, nil, fmt.Errorf("cannot create certificate: %w", err)
 	}
 
-	return caCertBytes, privateKey, nil
+	caCertPEM, err := pemEncodeCertificate(caCertBytes)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot encode certificate: %w", err)
+	}
+
+	privateKeyPEM, err := pemEncodePrivateKey(privateKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot encode private key: %w", err)
+	}
+
+	return caCertPEM, privateKeyPEM, nil
 }
 
-// GenerateCert generates a certificate and private key signed by the provided CA.
-// The certificate is valid for 10 years.
-func GenerateCert(ca []byte,
-	commonName string,
-	extraSANs []string,
-	caPrivateKey *rsa.PrivateKey,
-) ([]byte, *rsa.PrivateKey, error) {
-	caCertificate, err := x509.ParseCertificate(ca)
+// GenerateCert generates a certificate and private key signed by the provided CA in PEM format.
+// It accepts the CA root certificate and private key, validity bounds, and DNS name as parameters.
+func GenerateCert(caCertPEM []byte,
+	caPrivateKeyPEM []byte,
+	notBefore time.Time,
+	notAfter time.Time,
+	dnsName string,
+) ([]byte, []byte, error) {
+	caCertBlock, _ := pem.Decode(caCertPEM)
+	caCert, err := x509.ParseCertificate(caCertBlock.Bytes)
 	if err != nil {
-		return nil, nil, fmt.Errorf("error parsing certificate: %w", err)
+		return nil, nil, fmt.Errorf("error parsing ca root certificate: %w", err)
+	}
+
+	caPrivateKeyBlock, _ := pem.Decode(caPrivateKeyPEM)
+	caPrivateKey, err := x509.ParsePKCS1PrivateKey(caPrivateKeyBlock.Bytes)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error parsing ca root private key: %w", err)
 	}
 
 	serialNumber, err := rand.Int(rand.Reader, (&big.Int{}).Exp(big.NewInt(base), big.NewInt(exp), nil))
@@ -89,22 +105,10 @@ func GenerateCert(ca []byte,
 		return nil, nil, fmt.Errorf("cannot generate private key: %w", err)
 	}
 
-	sansHosts := []string{}
-	sansIps := []net.IP{}
-
-	for _, san := range extraSANs {
-		sanIP := net.ParseIP(san)
-		if sanIP == nil {
-			sansHosts = append(sansHosts, san)
-		} else {
-			sansIps = append(sansIps, sanIP)
-		}
-	}
-
 	cert := x509.Certificate{
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			CommonName:    commonName,
+			CommonName:    dnsName,
 			Organization:  []string{""},
 			Country:       []string{""},
 			Province:      []string{""},
@@ -112,30 +116,38 @@ func GenerateCert(ca []byte,
 			StreetAddress: []string{""},
 			PostalCode:    []string{""},
 		},
-		DNSNames:     sansHosts,
-		IPAddresses:  sansIps,
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(constants.CertExpirationYears, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
+		DNSNames:    []string{dnsName},
+		NotBefore:   notBefore,
+		NotAfter:    notAfter,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
 	}
 
 	certBytes, err := x509.CreateCertificate(
 		rand.Reader,
 		&cert,
-		caCertificate,
+		caCert,
 		&privateKey.PublicKey,
 		caPrivateKey)
 	if err != nil {
 		return nil, nil, fmt.Errorf("cannot create certificate: %w", err)
 	}
 
-	return certBytes, privateKey, nil
+	certPEM, err := pemEncodeCertificate(certBytes)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot encode certificate: %w", err)
+	}
+
+	privateKeyPEM, err := pemEncodePrivateKey(privateKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot encode private key: %w", err)
+	}
+
+	return certPEM, privateKeyPEM, nil
 }
 
-// PEMEncodeCertificate encodes a certificate to PEM format.
-func PEMEncodeCertificate(certificate []byte) ([]byte, error) {
+// pemEncodeCertificate encodes a certificate to PEM format.
+func pemEncodeCertificate(certificate []byte) ([]byte, error) {
 	certificatePEM := new(bytes.Buffer)
 
 	err := pem.Encode(certificatePEM, &pem.Block{
@@ -149,8 +161,8 @@ func PEMEncodeCertificate(certificate []byte) ([]byte, error) {
 	return certificatePEM.Bytes(), nil
 }
 
-// PEMEncodePrivateKey encodes a private key to PEM format.
-func PEMEncodePrivateKey(privateKey *rsa.PrivateKey) ([]byte, error) {
+// pemEncodePrivateKey encodes a private key to PEM format.
+func pemEncodePrivateKey(privateKey *rsa.PrivateKey) ([]byte, error) {
 	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
 	privateKeyPEM := new(bytes.Buffer)
 
@@ -163,4 +175,73 @@ func PEMEncodePrivateKey(privateKey *rsa.PrivateKey) ([]byte, error) {
 	}
 
 	return privateKeyPEM.Bytes(), nil
+}
+
+// NewCertPool creates a new x509.CertPool from a PEM-encoded certificate that may contain multiple certificates.
+func NewCertPool(certPEM []byte) (*x509.CertPool, error) {
+	certPool := x509.NewCertPool()
+
+	for {
+		var block *pem.Block
+		block, certPEM = pem.Decode(certPEM)
+		if block == nil {
+			break
+		}
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing certificate: %w", err)
+		}
+
+		certPool.AddCert(cert)
+	}
+
+	return certPool, nil
+}
+
+func VerifyCA(caCertPEM, caPrivateKeyPEM []byte, at time.Time) error {
+	pool, err := NewCertPool(caCertPEM)
+	if err != nil {
+		return fmt.Errorf("failed to create cert pool: %w", err)
+	}
+
+	if err = VerifyCert(caCertPEM, caPrivateKeyPEM, pool, "", at); err != nil {
+		return fmt.Errorf("failed to verify CA certificate: %w", err)
+	}
+
+	return nil
+}
+
+func VerifyCert(certPEM, privateKeyPEM []byte, certPool *x509.CertPool, dnsName string, at time.Time) error {
+	// Decode and parse the certificate
+	certBlock, _ := pem.Decode(certPEM)
+	cert, err := x509.ParseCertificate(certBlock.Bytes)
+	if err != nil {
+		return fmt.Errorf("error parsing certificate: %w", err)
+	}
+
+	// Verify the private key
+	if privateKeyPEM != nil {
+		if _, err = tls.X509KeyPair(certPEM, privateKeyPEM); err != nil {
+			return fmt.Errorf("key pair is invalid: %w", err)
+		}
+	}
+
+	// Set up the verification options
+	opts := x509.VerifyOptions{
+		Roots:       certPool,
+		DNSName:     dnsName,
+		CurrentTime: at,
+	}
+
+	// Verify the certificate
+	if _, err = cert.Verify(opts); err != nil {
+		return fmt.Errorf("the certificate is invalid: %w", err)
+	}
+
+	return nil
+}
+
+func DNSName(serviceName, namespace string) string {
+	return fmt.Sprintf("%s.%s.svc", serviceName, namespace)
 }
