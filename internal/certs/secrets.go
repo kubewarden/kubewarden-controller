28a74952c67cd0b8d449cd8f77b184b61d08a4a3
Fabrizio Sestito <fabrizio.sestito@suse.com>
2024-08-08 13:25:02 +0200

    chore: rename ok -> found
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/certs/secrets.go
+++ internal/certs/secrets.go
@@ -8,17 +8,18 @@ import (
 )
 
 // Extract the CA certificate and private key from a secret.
+// Both are PEM encoded.
 func ExtractCARootFromSecret(caRootSecret *corev1.Secret) ([]byte, []byte, error) {
-	caCert, ok := caRootSecret.Data[constants.CARootCert]
-	if !ok {
+	caCert, found := caRootSecret.Data[constants.CARootCert]
+	if !found {
 		return nil, nil, fmt.Errorf("CA could not be extracted from secret: %s", caRootSecret.GetName())
 	}
 	if len(caCert) == 0 {
 		return nil, nil, fmt.Errorf("CA certificate is empty in secret: %s", caRootSecret.GetName())
 	}
 
-	caPrivateKey, ok := caRootSecret.Data[constants.CARootPrivateKey]
-	if !ok {
+	caPrivateKey, found := caRootSecret.Data[constants.CARootPrivateKey]
+	if !found {
 		return nil, nil, fmt.Errorf("CA private key bytes could not be extracted from secret: %s", caRootSecret.GetName())
 	}
 	if len(caPrivateKey) == 0 {
@@ -29,17 +30,18 @@ func ExtractCARootFromSecret(caRootSecret *corev1.Secret) ([]byte, []byte, error
 }
 
 // Extract the server certificate and private key from a secret.
+// Both are PEM encoded.
 func ExtractServerCertFromSecret(serverCertSecret *corev1.Secret) ([]byte, []byte, error) {
-	serverCert, ok := serverCertSecret.Data[constants.ServerCert]
-	if !ok {
+	serverCert, found := serverCertSecret.Data[constants.ServerCert]
+	if !found {
 		return nil, nil, fmt.Errorf("server certificate could not be extracted from secret: %s", serverCertSecret.GetName())
 	}
 	if len(serverCert) == 0 {
 		return nil, nil, fmt.Errorf("server certificate is empty in secret: %s", serverCertSecret.GetName())
 	}
 
-	serverPrivateKey, ok := serverCertSecret.Data[constants.ServerPrivateKey]
-	if !ok {
+	serverPrivateKey, found := serverCertSecret.Data[constants.ServerPrivateKey]
+	if !found {
 		return nil, nil, fmt.Errorf("server private key could not be extracted from secret: %s", serverCertSecret.GetName())
 	}
 	if len(serverPrivateKey) == 0 {
