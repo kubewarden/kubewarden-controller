774f2849cd2defcf4022a6651ea8325e4d9e9237
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-01-21 12:30:30 +0100

    test: use PolicyServer factory in validation tests; add missing wire-up tests
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- api/policies/v1/policyserver_webhook_test.go
+++ api/policies/v1/policyserver_webhook_test.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/go-logr/logr"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -26,76 +27,119 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
 
 	"github.com/kubewarden/kubewarden-controller/internal/constants"
 )
 
-func TestDefault(t *testing.T) {
-	policyServer := &PolicyServer{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "policy-server",
-		},
-		Spec: PolicyServerSpec{
-			Limits: corev1.ResourceList{
-				"cpu":    resource.MustParse("100m"),
-				"memory": resource.MustParse("1Gi"),
-			},
-		},
-	}
+func TestPolicyServerDefault(t *testing.T) {
+	defaulter := policyServerDefaulter{}
+	policyServer := &PolicyServer{}
 
-	policyServerDefaulter := policyServerDefaulter{}
-	err := policyServerDefaulter.Default(context.Background(), policyServer)
+	err := defaulter.Default(context.Background(), policyServer)
 	require.NoError(t, err)
 
 	assert.Contains(t, policyServer.Finalizers, constants.KubewardenFinalizer)
 }
 
-func TestValidatePolicyServerName(t *testing.T) {
+func TestPolicyServerDefaultWithInvalidType(t *testing.T) {
+	policyServerDefaulter := policyServerDefaulter{}
+	obj := &corev1.Pod{}
+
+	err := policyServerDefaulter.Default(context.Background(), obj)
+	require.ErrorContains(t, err, "expected a PolicyServer object, got *v1.Pod")
+}
+
+func TestPolicyServerValidateCreate(t *testing.T) {
+	validator := policyServerValidator{logger: logr.Discard()}
+	policyServer := NewPolicyServerFactory().Build()
+
+	warnings, err := validator.ValidateCreate(context.Background(), policyServer)
+	require.NoError(t, err)
+	assert.Empty(t, warnings)
+}
+
+func TestPolicyServerValidateCreateWithErrors(t *testing.T) {
+	validator := policyServerValidator{logger: logr.Discard()}
+	policyServer := NewPolicyServerFactory().
+		WithMaxUnavailable(ptr.To(intstr.FromInt(2))).
+		WithMinAvailable(ptr.To(intstr.FromInt(2))).
+		Build()
+
+	warnings, err := validator.ValidateCreate(context.Background(), policyServer)
+	require.Error(t, err)
+	assert.Empty(t, warnings)
+}
+
+func TestPolicyServerValidateCreateWithInvalidType(t *testing.T) {
+	validator := policyServerValidator{logger: logr.Discard()}
+	obj := &corev1.Pod{}
+
+	warnings, err := validator.ValidateCreate(context.Background(), obj)
+	require.ErrorContains(t, err, "expected a PolicyServer object, got *v1.Pod")
+	assert.Empty(t, warnings)
+}
+
+func TestPolicyServerValidateUpdate(t *testing.T) {
+	validator := policyServerValidator{logger: logr.Discard()}
+	oldPolicyServer := NewPolicyServerFactory().Build()
+	newPolicyServer := NewPolicyServerFactory().
+		WithMaxUnavailable(ptr.To(intstr.FromInt(2))).
+		Build()
+
+	warnings, err := validator.ValidateUpdate(context.Background(), oldPolicyServer, newPolicyServer)
+	require.NoError(t, err)
+	assert.Empty(t, warnings)
+}
+
+func TestPolicyServerValidateUpdateWithErrors(t *testing.T) {
+	validator := policyServerValidator{logger: logr.Discard()}
+	oldPolicyServer := NewPolicyServerFactory().Build()
+	newPolicyServer := NewPolicyServerFactory().
+		WithMaxUnavailable(ptr.To(intstr.FromInt(2))).
+		WithMinAvailable(ptr.To(intstr.FromInt(2))).
+		Build()
+
+	warnings, err := validator.ValidateUpdate(context.Background(), oldPolicyServer, newPolicyServer)
+	require.Error(t, err)
+	assert.Empty(t, warnings)
+}
+
+func TestPolicyServerValidateUpdateWithInvalidType(t *testing.T) {
+	validator := policyServerValidator{logger: logr.Discard()}
+	obj := &corev1.Pod{}
+	newPolicyServer := NewPolicyServerFactory().Build()
+
+	warnings, err := validator.ValidateUpdate(context.Background(), newPolicyServer, obj)
+	require.ErrorContains(t, err, "expected a PolicyServer object, got *v1.Pod")
+	assert.Empty(t, warnings)
+}
+
+func TestPolicyServerValidateName(t *testing.T) {
 	name := make([]byte, 64)
 	for i := range name {
 		name[i] = 'a'
 	}
-	policyServer := &PolicyServer{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      string(name),
-			Namespace: "default",
-		},
-		Spec: PolicyServerSpec{
-			Image:    "image",
-			Replicas: 1,
-		},
-	}
-	policyServerValidator := policyServerValidator{
-		k8sClient:            nil,
-		deploymentsNamespace: "default",
-	}
+	policyServer := NewPolicyServerFactory().WithName(string(name)).Build()
+
+	policyServerValidator := policyServerValidator{logger: logr.Discard()}
 	err := policyServerValidator.validate(context.Background(), policyServer)
 	require.ErrorContains(t, err, "the PolicyServer name cannot be longer than 63 characters")
 }
 
-func TestValidateMinAvailable(t *testing.T) {
-	intStrValue := intstr.FromInt(2)
-	policyServer := &PolicyServer{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "policy-server",
-			Namespace: "default",
-		},
-		Spec: PolicyServerSpec{
-			Image:          "image",
-			Replicas:       1,
-			MinAvailable:   &intStrValue,
-			MaxUnavailable: &intStrValue,
-		},
-	}
-	policyServerValidator := policyServerValidator{
-		k8sClient:            nil,
-		deploymentsNamespace: "default",
-	}
+func TestPolicyServerValidateMinAvailableMaxUnavailable(t *testing.T) {
+	policyServer := NewPolicyServerFactory().
+		WithMinAvailable(ptr.To(intstr.FromInt(2))).
+		WithMaxUnavailable(ptr.To(intstr.FromInt(2))).
+		Build()
+
+	policyServerValidator := policyServerValidator{logger: logr.Discard()}
+
 	err := policyServerValidator.validate(context.Background(), policyServer)
 	require.ErrorContains(t, err, "minAvailable and maxUnavailable cannot be both set")
 }
 
-func TestValidateImagePullSecret(t *testing.T) {
+func TestPolicyServerValidateImagePullSecret(t *testing.T) {
 	tests := []struct {
 		name   string
 		secret *corev1.Secret
@@ -134,25 +178,20 @@ func TestValidateImagePullSecret(t *testing.T) {
 			k8sClient := fake.NewClientBuilder().Build()
 
 			if test.secret != nil {
-				err := k8sClient.Create(context.TODO(), test.secret)
+				err := k8sClient.Create(context.Background(), test.secret)
 				if err != nil {
 					t.Errorf("failed to create secret: %s", err.Error())
 				}
 			}
 
-			policyServer := &PolicyServer{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "policy-server",
-					Namespace: "default",
-				},
-				Spec: PolicyServerSpec{
-					ImagePullSecret: "test",
-				},
-			}
+			policyServer := NewPolicyServerFactory().
+				WithImagePullSecret("test").
+				Build()
 
 			policyServerValidator := policyServerValidator{
-				k8sClient:            k8sClient,
 				deploymentsNamespace: "default",
+				k8sClient:            k8sClient,
+				logger:               logr.Discard(),
 			}
 			err := policyServerValidator.validate(context.Background(), policyServer)
 
@@ -165,7 +204,7 @@ func TestValidateImagePullSecret(t *testing.T) {
 	}
 }
 
-func TestValidateLimitsAndRequests(t *testing.T) {
+func TestPolicyServerValidateLimitsAndRequests(t *testing.T) {
 	tests := []struct {
 		name     string
 		limits   corev1.ResourceList
@@ -200,16 +239,12 @@ func TestValidateLimitsAndRequests(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			policyServer := &PolicyServer{
-				Spec: PolicyServerSpec{
-					Limits:   test.limits,
-					Requests: test.requests,
-				},
-			}
-			policyServerValidator := policyServerValidator{
-				k8sClient:            nil,
-				deploymentsNamespace: "default",
-			}
+			policyServer := NewPolicyServerFactory().
+				WithLimits(test.limits).
+				WithRequests(test.requests).
+				Build()
+
+			policyServerValidator := policyServerValidator{logger: logr.Discard()}
 			err := policyServerValidator.validate(context.Background(), policyServer)
 
 			if test.error != "" {
