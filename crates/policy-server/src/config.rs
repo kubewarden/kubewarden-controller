04fe1a42d91efa4e6e887a8b04e816577649e3fc
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2025-07-17 12:56:33 +0200

    refactor: Delete AdmissionResponseHandler{},PolicyMode{},PolicyID{},EvaluationError{}
    Their implementations have been moved to policy-evaluator together with
    its history.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- crates/policy-server/src/config.rs
+++ crates/policy-server/src/config.rs
@@ -285,24 +285,6 @@ fn remote_server_options(matches: &clap::ArgMatches) -> Result<Option<Sources>> 
     Ok(sources)
 }
 
-#[derive(Deserialize, Debug, Clone, Default, PartialEq)]
-pub enum PolicyMode {
-    #[serde(rename = "monitor")]
-    Monitor,
-    #[serde(rename = "protect")]
-    #[default]
-    Protect,
-}
-
-impl From<PolicyMode> for String {
-    fn from(policy_mode: PolicyMode) -> String {
-        match policy_mode {
-            PolicyMode::Monitor => String::from("monitor"),
-            PolicyMode::Protect => String::from("protect"),
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 pub enum PolicyOrPolicyGroupSettings {
     Policy(PolicySettings),
