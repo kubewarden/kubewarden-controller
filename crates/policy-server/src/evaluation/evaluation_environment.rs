b54e6f89769c3ba1491ffebdfb2287b778980141
José Guilherme Vanz <jguilhermevanz@suse.com>
2025-08-18 13:40:48 -0300

    fix(linter): remove struct not used.
    Removes a struct not used. This was causing errors with Golang linter.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- crates/policy-server/src/evaluation/evaluation_environment.rs
+++ crates/policy-server/src/evaluation/evaluation_environment.rs
@@ -1,6 +1,5 @@
 use std::{
     collections::{BTreeSet, HashMap, HashSet},
-    fmt,
     sync::Arc,
 };
 
@@ -33,38 +32,6 @@ use crate::{
 
 #[cfg(test)]
 use mockall::automock;
-
-/// This holds the a summary of the evaluation results of a policy group member
-struct PolicyGroupMemberEvaluationResult {
-    /// whether the request is allowed or not
-    allowed: bool,
-    /// the optional message included inside of the evaluation result of the policy
-    message: Option<String>,
-}
-
-impl From<AdmissionResponse> for PolicyGroupMemberEvaluationResult {
-    fn from(response: AdmissionResponse) -> Self {
-        Self {
-            allowed: response.allowed,
-            message: response.status.and_then(|status| status.message),
-        }
-    }
-}
-
-impl fmt::Display for PolicyGroupMemberEvaluationResult {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.allowed {
-            write!(f, "[ALLOWED]")?;
-        } else {
-            write!(f, "[DENIED]")?;
-        }
-        if let Some(message) = &self.message {
-            write!(f, " - {}", message)?;
-        }
-
-        Ok(())
-    }
-}
 
 /// The digest of a WebAssembly module
 type ModuleDigest = String;
