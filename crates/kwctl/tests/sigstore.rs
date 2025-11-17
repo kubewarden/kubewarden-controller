956dd4d64b63f44b4c624c1bdf5798c5a943d007
José Guilherme Vanz <jguilhermevanz@suse.com>
2025-11-17 15:07:16 -0300

    feat(e2e): test sigstore-trust-config CLI flag.
    Adds a e2e test to validate the --sigstore-trust-config CLI flag. This
    test is behind a feature flag to avoid running it in a environment with
    no local Sigstore running.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

---
+++ crates/kwctl/tests/sigstore.rs
@@ -0,0 +1,52 @@
+#[cfg(feature = "sigstore-testing")]
+use common::setup_command;
+#[cfg(feature = "sigstore-testing")]
+use tempfile::tempdir;
+
+mod common;
+
+/// This test is behind a feature flag because it requires a sigstore testing environment to run
+/// properly. In CI this is done by the sigstore/scaffolding/actions/setup action and some
+/// additional configuration. Locally, one can use the script and documentation available in the
+/// same repository.
+///
+/// Furthermore, this test expects that there is a policy
+/// "registry.local:5000/policies/testing:latest" is signed with cosign in the local sigstore
+/// instance. It also expects that the sigstore trust_config.json and verification_config.yaml files
+/// are available. The trust_config.json file should contain all the information to find the
+/// local sigstore instance. It follows the ClientTrustConfig format. See the spec here:
+/// https://github.com/sigstore/protobuf-specs/blob/4d38e4482bf67c7ab86bf2f61e8d79010ac0974e/protos/sigstore_trustroot.proto#L341
+/// The verification_config.yaml file should contain the verification configuration for the policy,
+/// it can be generated using `kwctl scaffold verification-config` command. The verification
+/// options in the file should match the way the policy was signed in the local sigstore instance.
+/// The test also checks if there is a sources.yaml files. If it exists, it is copied to the temp
+/// directory and the command is run with the --sources-path argument.
+#[test]
+#[cfg(feature = "sigstore-testing")]
+fn test_sigstore_trust_config() {
+    let tempdir = tempdir().unwrap();
+    let trust_config_file = tempdir.path().join("trust_config.json");
+    std::fs::copy("trust_config.json", &trust_config_file).expect("cannot copy trust_config.json");
+    let verification_config_file = tempdir.path().join("verification_config.yaml");
+    std::fs::copy("verification_config.yaml", &verification_config_file)
+        .expect("cannot copy verification_config.yaml");
+
+    let mut cmd = setup_command(tempdir.path());
+    cmd.arg("verify")
+        .arg("--sigstore-trust-config")
+        .arg("trust_config.json")
+        .arg("--verification-config-path")
+        .arg("verification_config.yaml");
+
+    // if there is a sources.yaml file, the test consider that the registry does not support https
+    // request. Therefore, the sources file should be copied to the tempdir as well and the command
+    // should be run with the --sources-path argument.
+    if std::path::Path::new("sources.yaml").exists() {
+        let sources_file = tempdir.path().join("sources.yaml");
+        std::fs::copy("sources.yaml", &sources_file).expect("cannot copy sources.yaml");
+        cmd.arg("--sources-path").arg("sources.yaml");
+    }
+
+    cmd.arg("registry://registry.local:5000/policies/testing:latest");
+    cmd.assert().success();
+}
